f35e68f5cd2ec2cf101bf4d75da7871c903c39a7
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-04-17 02:33:51 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,7 +1,6 @@
 function solution(arr, idx) {
-    let result = []
     for (let i = idx; i <= arr.length; i++){
-        if (arr[i] == 1) result.push(i) 
+        if (arr[i] == 1) return i
     }
-    return result.length == 0? -1 : result[0] 
+    return -1
 }
