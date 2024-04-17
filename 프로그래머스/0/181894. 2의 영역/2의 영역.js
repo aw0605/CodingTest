a946dd1547ad368c3c764fb43edad6acd293a6a3
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-04-17 23:21:53 +0900

    [level 0] Title: 2의 영역, Time: 0.35 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -2,5 +2,5 @@ function solution(arr) {
     let slice1 = arr.indexOf(2)
     let slice2 = arr.lastIndexOf(2)
 
-    return slice1 == -1 || slice2 == -1? [-1] : arr.slice(slice1, slice2+1)
+    return slice1 == -1? [-1] : arr.slice(slice1, slice2+1)
 }
