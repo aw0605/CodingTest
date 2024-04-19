b8ccfa434a2b9aa17dff1a9805742c3310f602f2
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-04-19 23:22:25 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -0,0 +1,7 @@
+function solution(str_list) {
+    let idx = str_list.findIndex(el => el === "l" || el === "r")
+    if (idx === -1) return []
+    else if (str_list[idx] === "l"){
+        return str_list.slice(0,idx)
+    } else {return str_list.slice(idx+1)}
+}
