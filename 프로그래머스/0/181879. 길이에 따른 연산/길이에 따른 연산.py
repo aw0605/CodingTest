c09bde647b25cd89caa8fea17905b5b1278205c4
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-04-23 02:46:43 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -1,4 +1,3 @@
 from functools import reduce
 def solution(num_list):
-    func = lambda a, b: a + b if (len(num_list) >= 11) else a * b
-    return reduce(func, num_list)
+    return sum(num_list) if len(num_list) >= 11 else reduce(lambda a,b: a*b, num_list)
