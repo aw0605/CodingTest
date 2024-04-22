f51a2d25e9e8a2516a9fff9c56bf297bcdb7d1b2
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-04-23 02:30:07 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -2,7 +2,7 @@ def solution(num_list):
     answer = 0
     for v in num_list:
         while v != 1:
-            if v % 2 == 0: v = v // 2
-            else: v = (v - 1) // 2
+            if v % 2 == 0: v /= 2
+            else: v = (v - 1) / 2
             answer += 1
     return answer
