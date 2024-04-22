f16872a4ab9931e838681acd1ba62475935981d3
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-04-22 19:44:03 +0900

    [level 0] Title: 5명씩, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.js
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -1,7 +1,7 @@
 function solution(names) {
     var answer = [];
     for (let i = 0; i < names.length; i+=5){
-        answer.push(names.slice(i, i+4)[0])
+        answer.push(names[i])
     }
     return answer;
 }
