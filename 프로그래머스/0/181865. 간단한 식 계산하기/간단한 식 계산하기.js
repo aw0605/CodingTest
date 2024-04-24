7bbc946c8c26e50020ef12ad527aa218ba86772a
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-04-24 14:25:52 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,10 @@
+function solution(binomial) {
+    let [a, op, b] = binomial.split(" ");
+    a = +a;
+    b = +b;
+    switch(op){
+        case "+": return a + b;
+        case "-": return a - b;
+        case "*": return a * b;
+    }
+}
