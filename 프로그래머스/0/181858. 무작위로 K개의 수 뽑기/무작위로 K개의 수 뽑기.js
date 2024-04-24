5b4d3b0237c6b60ae49a5738b9ce683117e65be1
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-04-25 03:19:14 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 0.77 ms, Memory: 34.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -0,0 +1,11 @@
+function solution(arr, k) {
+    var answer = [];
+    for(let i = 0; i < arr.length; i++){
+        if(answer.length === k) return answer;
+        if(!answer.includes(arr[i])) answer.push(arr[i]);   
+    }
+    while(answer.length < k){
+        answer.push(-1);
+    }
+    return answer;
+}
