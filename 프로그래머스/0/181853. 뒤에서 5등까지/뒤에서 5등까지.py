bce617277b3e40b0670ff73fa200add17416d8b2
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-04-25 22:46:40 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.py
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.py
@@ -1,6 +1,2 @@
 def solution(num_list):
-    answer = []
-    for i in range(5):
-        answer.append(min(num_list))
-        num_list.remove(min(num_list))
-    return answer
+    return sorted(num_list)[:5]
