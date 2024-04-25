80a627eb297eae96c466caaa56d244497af6f9eb
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-04-25 15:01:50 +0900

    [level 0] Title: 배열 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
@@ -0,0 +1,7 @@
+def solution(arr1, arr2):
+    len1, len2 = len(arr1), len(arr2)
+    if len1 != len2: return 1 if len1 > len2 else -1
+    else: 
+        sum1, sum2 = sum(arr1), sum(arr2)
+        if sum1 == sum2: return 0
+        else: return 1 if sum1 > sum2 else -1
