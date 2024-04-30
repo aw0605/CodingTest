fb6921e41bc058c55b8e09fd44b22f33e80cf93c
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-04-30 15:10:35 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,7 +1,9 @@
+from collections import Counter
+
 def solution(array):
-    answerArr = [0] * (max(array)+1)
-    for v in array:
-        answerArr[v] += 1
-    
-    if answerArr.count(max(answerArr)) != 1: return -1 
-    else: return answerArr.index(max(answerArr))
+    a = Counter(array).most_common(2)
+    if len(a) == 1:
+        return a[0][0]
+    if a[0][1] == a[1][1]:
+        return -1
+    return a[0][0]
