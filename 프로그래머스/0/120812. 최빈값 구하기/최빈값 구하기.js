c88144738b79a9a66ccb8304b871dd489845f908
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-04-30 13:46:22 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,13 +1,15 @@
 function solution(array) {
-    let newArray = new Array(Math.max(...array)+1).fill(0);
+    let answerArr = new Array(Math.max(...array)+1).fill(0);
     
     for (let i = 0; i < array.length; i++) {
-        newArray[array[i]] += 1;
+        answerArr[array[i]] += 1;
     }
     
-    if (newArray.indexOf(Math.max(...newArray)) !== newArray.lastIndexOf(Math.max(...newArray))) {
+    let mode = Math.max(...answerArr)
+    
+    if (answerArr.indexOf(mode) !== answerArr.lastIndexOf(mode)) {
         return -1
     } else {
-        return newArray.indexOf(Math.max(...newArray));
+        return answerArr.indexOf(mode);
     };
 }
