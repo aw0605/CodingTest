0479c2a12a9477e05b0ec0044aa54269172ae315
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-03 03:53:19 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,7 +1,2 @@
 def solution(rsp):
-    arr = {
-        "2":"0",
-        "0":"5",
-        "5":"2"
-    }
-    return ''.join([arr[v] for v in rsp])
+    return rsp.translate(str.maketrans('025', '502'))
