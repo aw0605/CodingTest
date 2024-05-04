efd87b162700553475bb7224da3994d6ccb4f1d8
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-04 21:57:31 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,7 +1,7 @@
 function solution(num_list, n) {
-    let answer = [];
-    for (let i = 0; i < num_list.length; i += n){
-        answer.push(num_list.slice(i, i+n));
+    var answer = [];
+    while(num_list.length) {
+        answer.push(num_list.splice(0,n));
     }
     return answer;
 }
