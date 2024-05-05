ee121ef2b364cdf9a3439dac84cae5dd94649cc5
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-05 23:50:33 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,4 +1,3 @@
 function solution(my_string) {
-    let numArr = [...my_string].filter(v => Math.floor(v) || Math.floor(v) === 0)
-    return numArr.map(v => +v).sort((a, b) => a - b);
+    return [...my_string].filter((v) => !isNaN(v)).map((v) => +v).sort((a,b) => a-b)
 }
