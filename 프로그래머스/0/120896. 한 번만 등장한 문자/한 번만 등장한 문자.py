ad9e8ea1c56a2f505b3afce094b388d003112ac4
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-08 19:40:22 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,5 +1,2 @@
 def solution(s):
-    answer = []
-    for v in s:
-        if s.index(v) == s.rindex(v): answer.append(v)
-    return ''.join(sorted(answer))
+    return "".join(sorted([ v for v in s if s.count(v) == 1]))
