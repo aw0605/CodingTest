08d45a92b86b571e1d2fb915d12ad9012aa3e881
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-08 20:54:23 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,9 +1,2 @@
 def solution(my_string):
-    strArr = my_string.split()
-    answer = int(strArr[0])
-    for i in range(1,len(strArr),2):
-        if strArr[i] == '+':
-            answer += int(strArr[i+1])
-        else:
-            answer -= int(strArr[i+1])
-    return answer
+    return sum(int(i) for i in my_string.replace(' - ', ' + -').split(' + '))
