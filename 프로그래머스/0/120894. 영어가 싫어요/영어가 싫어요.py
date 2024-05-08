5cf1bde63db323ba85603b7b7abe5c427704778b
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-08 19:28:34 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,4 @@
+def solution(numbers):
+    for i, v in enumerate(["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]):
+        numbers = numbers.replace(v, str(i))
+    return int(numbers)
