8fd7645efef42cf896ae497eca87da6d4974e851
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-10 22:15:31 +0900

    [level 0] Title: 다항식 더하기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,13 +1,16 @@
 function solution(polynomial) {
-    let xnum = 0;
-    let cnum = 0;
-    for(v of polynomial.split(' + ')){
-        if (!isNaN(v)) cnum += +v
-        else v === "x"? xnum++ : xnum += +v.slice(0, v.length - 1)
-    }
-    
-    if (xnum === 0) {return cnum.toString()}
-    else if (xnum === 1) {
-        return cnum !== 0? `x + ${cnum}` : "x"
-    } else return cnum !== 0? `${xnum}x + ${cnum}` : `${xnum}x`
+    const arr = polynomial.split(" + ");
+    const xNum = arr
+                .filter(n => n.includes("x"))
+                .map(n => n.replace('x', '') || '1')
+                .reduce((acc, cur) => acc + parseInt(cur), 0);
+    const num = arr
+                .filter(n => !isNaN(n))
+                .reduce((acc, cur) => acc + parseInt(cur), 0);
+
+    let answer = [];
+    if(xNum) answer.push(`${xNum === 1 ? "" : xNum}x`);
+    if(num) answer.push(num);
+
+    return answer.join(" + ");
 }
