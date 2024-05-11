54c80a0ff7a83cb70bbb2f21553ed186847c5c60
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-12 00:22:26 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -2,11 +2,8 @@ function solution(sides) {
     let answer = 0
     const max = Math.max(...sides)
     const min = Math.min(...sides)
-    for(let i = max-min+1; i <= max; i++) {
+    for(let i = max-min+1; i <= max+min; i++) {
         answer++
     }
-    for(let i = max+1; i < max+min; i++) {
-        answer++
-    }
-    return answer;
+    return answer - 1;
 }
