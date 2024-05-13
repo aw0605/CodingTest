93096803ddb1fd556985408be665dc888fa65e4b
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-14 01:42:30 +0900

    [level 0] Title: k의 개수, Time: 9.51 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/0/120887. k의 개수/k의 개수.py
@@ -1,4 +1,3 @@
 def solution(i, j, k):
-    answer = ""
-    for v in range(i,j+1): answer += str(v)
-    return len(answer.split(str(k))) - 1
+    answer = sum([ str(i).count(str(k)) for i in range(i,j+1)])
+    return answer
