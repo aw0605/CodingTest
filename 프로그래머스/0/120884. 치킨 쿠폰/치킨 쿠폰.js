71e56b7a663f5c09f82502866c4d02433be71d08
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-14 03:59:07 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,10 +1,3 @@
 function solution(chicken) {
-    let answer = 0;
-    
-    while (chicken > 9) {
-        answer += Math.trunc(chicken / 10);
-        chicken = chicken % 10 + Math.trunc(chicken / 10)
-    }
-    
-    return answer;
+    return Math.trunc((chicken - 10) / 9 + 1)
 }
