2566a2354eee914bb8995f874c726dcc5c70cf4c
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-15 01:32:15 +0900

    [level 0] Title: 문자열 밀기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,6 +1,1 @@
-def solution(A, B):
-    if A == B: return 0
-    for i in range(1, len(A)):
-        A = A[-1] + A[:-1]
-        if A == B: return i
-    return -1
+solution = lambda A, B: (B*2).find(A)
