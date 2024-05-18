3f905098e4bb2c19f8893d7801941a0275ba35e9
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-19 01:26:49 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,15 +1,8 @@
 function solution(n, m) {
-    let gcd;
-    let lcm;
-    // 최대공약수
-    for (let i = Math.min(n,m); i >= 1; i--){
-        if ((n % i) === 0 && (m % i) === 0){
-            gcd = i;
-            break;
-        }
-    }
-    // 최소공배수
-    lcm = (n * m) / gcd
-    
-    return [gcd, lcm];
+    let r;  //나머지 저장 변수
+    let nm = n * m;
+    for (r = n % m; r !== 0; n = m, m = r, r = n % m) {}  
+    // 유클리드 호제법으로 최대공약수 계산
+    // 두수 n,m의 최대공약수 = m과 n % m의 최대 공약수
+    return [m, nm / m];
 }
