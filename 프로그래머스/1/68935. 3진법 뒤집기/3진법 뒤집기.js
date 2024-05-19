4964fa8cc6d17cdd166ed862d5e1c4edf3ff6ca0
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-19 11:49:55 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,12 +1,3 @@
 function solution(n) {
-    let threeNR = [];
-    let answer = 0;
-    while (n / 3 !== 0){
-        threeNR.push(n % 3)
-        n = Math.trunc(n / 3)
-    }
-    for(let i = 0; i < threeNR.length; i++){
-        answer += ([...threeNR].reverse())[i] * 3**i
-    }
-    return answer;
+    return parseInt([...n.toString(3)].reverse().join(""),3);
 }
