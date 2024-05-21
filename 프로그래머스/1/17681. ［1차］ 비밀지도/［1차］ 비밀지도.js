2de5e9f00743c590ed6f1d34f56777b40784cbae
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-21 23:56:12 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,14 +1,3 @@
 function solution(n, arr1, arr2) {
-    let answer = new Array(n).fill("");
-
-    for (let i = 0; i < n; i++){
-        let a1V = arr1[i].toString(2).padStart(n, "0")
-        let a2V = arr2[i].toString(2).padStart(n, "0")
-        
-        for (let j = 0; j < n; j++){
-            (a1V[j] === "1" || a2V[j] === "1")? answer[i] += "#" : answer[i] += " "
-        }
-
-    }
-    return answer;
+    return arr1.map((a1V,i)=>((a1V|arr2[i]).toString(2).padStart(n,"0").replace(/0/g," ").replace(/1/g,"#")))
 }
