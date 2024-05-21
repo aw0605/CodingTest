5d43dbf3a52a75c41e0dd2559ff89e40124e1b75
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-22 00:54:10 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,3 +1,3 @@
 function solution(array, commands) {
-    return commands.map((v) => (array.slice(v[0]-1,v[1]).sort((a,b) => a-b))[v[2]-1]);
+    return commands.map(([i,j,k]) => (array.slice(i-1,j).sort((a,b) => a-b))[k-1]);
 }
