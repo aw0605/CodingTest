3ffe4f300cde4c46726325d7dc2f0332694f196e
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-24 02:31:22 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,8 +1,9 @@
 function solution(a, b) {
-    const day = ['THU','FRI','SAT','SUN','MON','TUE','WED'];
-    const month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    let daySum;
-    if(a == 1) daySum = b;
-    else daySum = month.slice(0, a - 1).reduce((a,c) => a + c) + b;
-    return day[daySum%7];
+    const month = [31,29,31,30,31,30,31,31,30,31,30,31]
+    const day = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
+
+    let days = b
+    for(let i = 0; i < a-1; i++){days += month[i];}
+
+    return day[days%7];
 }
