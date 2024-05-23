d203b5f514aec52327ae59a1fa0333ccd78c2971
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-24 00:31:00 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,9 +1,6 @@
 def solution(cards1, cards2, goal):
     for v in goal:
-        if v in cards1:
-            if v == cards1[0]: cards1.pop(0)
-            else: return "No"
-        elif v in cards2:
-            if v == cards2[0]: cards2.pop(0)
-            else: return "No"
+        if len(cards1) > 0 and v == cards1[0]: cards1.pop(0)
+        elif len(cards2) > 0 and v == cards2[0]: cards2.pop(0)
+        else: return "No"
     return "Yes"
