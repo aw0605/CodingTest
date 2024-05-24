5c6552d82b5e2131f97b05dca084a84e01ad2659
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-24 19:37:34 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,6 +1,2 @@
 def solution(k, m, score):
-    answer = 0
-    score.sort(reverse=True)
-    for i in range(0,len(score)-(len(score)%m),m):
-        answer += min(score[i:i+m]) * m
-    return answer
+    return sum(sorted(score)[len(score)%m::m])*m
