664dcfc39226a9b07576b850b45f7fcbb63f85ce
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-27 21:47:39 +0900

    [level 1] Title: 덧칠하기, Time: 4.81 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,12 +1,14 @@
 function solution(n, m, section) {
     let answer = 0;
-    let paint = 0;
-    
-    for (let v of section){
-        if (paint < v){
+    const arr = new Array(n).fill(1);
+
+    section.forEach(v => arr[v-1] = 0);
+
+    section.map(v => {
+        if (!arr[v-1]) {
+            arr.fill(1, v-1, v+m-1);
             answer++
-            paint = v + m - 1
         }
-    }
-    return answer;
+    })
+    return answer
 }
