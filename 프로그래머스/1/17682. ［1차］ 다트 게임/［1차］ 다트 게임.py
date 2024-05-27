1cf8a6848126a3856e9ce74966322bbf2c351020
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-28 02:14:40 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.15 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,16 +1,15 @@
+import re
+
 def solution(dartResult):
-    score = ""
-    answer = []
-    SDTObj = {"S": 1,"D": 2,"T": 3}
+    bonus = {'S' : 1, 'D' : 2, 'T' : 3}
+    option = {'' : 1, '*' : 2, '#' : -1}
+    # 하나이상의 숫자 / SDT 중 하나 / *#없음 중 하나
+    p = re.compile('(\d+)([SDT])([*#]?)')
+    # 정규 표현식과 일치하는 부분 찾아 리스트로 반환 ["1","D",""]형태
+    dart = p.findall(dartResult)
     
-    for v in dartResult:
-        if v.isdigit(): score += v
-        elif v.isalpha(): 
-            answer.append(int(score) ** SDTObj[v])
-            score = ""
-        elif v == "*":
-            if len(answer) > 1: answer[-2] *= 2
-            answer[-1] *= 2
-        elif v == "#": answer[-1] *= -1
-    
-    return sum(answer)
+    for i in range(len(dart)):
+        if dart[i][2] == '*' and i > 0: dart[i-1] *= 2
+        dart[i] = int(dart[i][0]) ** bonus[dart[i][1]] * option[dart[i][2]]
+
+    return sum(dart)
