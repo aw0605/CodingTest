c5b1d29ffb4960f29f773950349569e48e6a36d4
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-28 00:30:41 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,18 +1,10 @@
 function solution(N, stages) {
     let answer = [];
-    let player = stages.length;
-    let clearArr = new Array(N+1).fill(0);
-    
-    for (let v of stages) clearArr[v-1]++
-    
-    for (let i = 0; i < N; i++){
-        if (player === 0) answer.push([i+1, 0]);
-        else answer.push([i+1, clearArr[i] / player]);
-        player -= clearArr[i]
+    for(let i = 1; i <= N; i++){
+        let player = stages.filter((x) => x >= i).length;
+        let fail = stages.filter((x) => x === i).length;
+        answer.push([i, fail/player]);
     }
-    
-    return answer.sort((a,b) => {
-        if(b[1] !== a[1]) return b[1] - a[1];
-        else return a[0] - b[0]
-    }).map(v => v[0]);
+    answer.sort((a,b) => b[1] - a[1]);
+    return answer.map((x) => x[0]);
 }
