c6ab945d466e4a60d013b273c87a4e0acd53e176
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-28 02:03:37 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/README.md
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 다트 게임 - 17682 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17682) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17682?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.17 ms
+메모리: 33.4 MB, 시간: 0.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 28일 01:42:26
+2024년 05월 28일 02:03:35
 
 ### 문제 설명
 
--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,21 +1,17 @@
 function solution(dartResult) {
-    let answer = [];
-    let score = "";
-    const SDTObj = {
-        "S": 1,
-        "D": 2,
-        "T": 3
+    const bonus = { 'S': 1, 'D': 2, 'T': 3 },
+          options = { '*': 2, '#': -1, undefined: 1 };
+
+    let darts = dartResult.match(/\d.?\D/g);
+
+    for (let i = 0; i < darts.length; i++) {
+        let split = darts[i].match(/(^\d{1,})(S|D|T)(\*|#)?/),
+            score = Math.pow(split[1], bonus[split[2]]) * options[split[3]];
+
+        if (split[3] === '*' && darts[i - 1]) darts[i - 1] *= options['*'];
+
+        darts[i] = score;
     }
-    
-    for (let v of dartResult){
-        if (!isNaN(v)) score += v
-        else if (/S|D|T/.test(v)) {
-            answer.push(parseInt(score) ** SDTObj[v])
-            score = ""
-        } else if (v === "*") {
-            if (answer.length > 1) answer[answer.length-2] *= 2
-            answer[answer.length-1] *= 2
-        } else if (v === "#") answer[answer.length-1] *= -1
-    }
-    return answer.reduce((a,c) => a + c, 0);
+
+    return darts.reduce((a, b) => a + b);
 }
