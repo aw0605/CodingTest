e87c11e5f11d14634ab228f8057820e3cd3295f9
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-30 02:15:36 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/README.md
+++ 프로그래머스/1/160586. 대충 만든 자판/README.md
@@ -1,10 +1,10 @@
 # [level 1] 대충 만든 자판 - 160586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586?language=python3) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 1.01 ms
+메모리: 10.4 MB, 시간: 1.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 30일 01:56:16
+2024년 05월 30일 02:15:34
 
 ### 문제 설명
 
--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,18 +1,17 @@
-def solution(keymap, targets):
+def solution(keymap, targets):  
     answer = []
-    for target in targets:
-        totalCount = 0;
-        for w in target:
-            count = 101;
-            contain = 0;
-            for key in keymap:
-                if w in key:
-                    count = min(count, key.index(w)+1)
-                    contain = 1
-            if not contain: 
-                totalCount = -1
-                break
-            totalCount += count
-        answer.append(totalCount)
+    keys = {}
+    for key in keymap:
+        for i,v in enumerate(key):
+            keys[v] = min(keys[v], i + 1) if v in keys else i + 1
             
+    for i,target in enumerate(targets):
+        total = 0
+        for w in target:
+            if w not in keys: 
+                total = -1
+                break
+            total += keys[w]
+        answer.append(total)
+        
     return answer
