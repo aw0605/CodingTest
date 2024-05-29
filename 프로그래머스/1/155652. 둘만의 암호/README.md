a7e7e324849eb2df9d966ed29db9f4ff2017710b
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-29 23:44:01 +0900

    [level 1] Title: 둘만의 암호, Time: 0.15 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652) 
 
 ### 성능 요약
 
-메모리: 33.1 MB, 시간: 0.21 ms
+메모리: 33.3 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 29일 15:00:04
+2024년 05월 29일 23:43:59
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,11 +1,6 @@
 function solution(s, skip, index) {
-    let answer = '';
-    const atoz = 'abcdefghijklmnopqrstuvwxyz'.match(
-        new RegExp(`[^${skip}]`, 'g'),
-    );
-    for (const w of s) {
-        const newIdx = atoz.indexOf(w) + index;
-        answer += atoz[newIdx % atoz.length];
-    }
-  return answer;
+    const alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'].filter(v => !skip.includes(v));
+
+    return [...s].reduce((a,c) => a += alpha[(alpha.indexOf(c)+index)%alpha.length], "");
 }
