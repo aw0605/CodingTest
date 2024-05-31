356102ce427d92014ff36f5e847e1573817e48fe
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-31 17:49:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 29.40 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,10 +1,15 @@
 function solution(participant, completion) {
-    let answer = '';
-    const pArr = participant.sort()
-    const cArr = completion.sort()
-    
-    for (let i = 0; i < pArr.length; i++){
-        if (pArr[i] !== cArr[i]) 
-            return pArr[i]
+    const map = new Map();
+
+    for(let i = 0; i < participant.length; i++) {
+        let a = participant[i], 
+            b = completion[i];
+
+        map.set(a, (map.get(a) || 0) + 1);
+        map.set(b, (map.get(b) || 0) - 1);
+    }
+
+    for(let [k, v] of map) {
+        if(v) return k;
     }
 }
