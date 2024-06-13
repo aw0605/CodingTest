700a453d50a7c34ce2acaff0c3247cb3eb0119b5
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-13 15:51:20 +0900

    [level 2] Title: 올바른 괄호, Time: 8.32 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -2,8 +2,10 @@ def solution(s):
     stack = []
     
     for v in s:
-        if len(stack) == 0: stack.append(v)
-        elif stack[-1] == "(" and v == ")": stack.pop()
-        else: stack.append(v)
+        if v == '(': stack.append(v)
 
-    return False if len(stack) else True
+        if v == ')':
+            try: stack.pop()
+            except IndexError: return False
+
+    return len(stack) == 0
