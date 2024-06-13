917dcad2d96751b56ff6672c7dc19dea95b56396
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-13 15:40:34 +0900

    [level 2] Title: 올바른 괄호, Time: 6.86 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,10 @@
+function solution(s){
+    let stack = []
+
+    for (let v of s){
+        if(stack[stack.length-1] == "(" && v == ")") stack.pop();
+        else stack.push(v)
+    }
+
+    return stack.length > 0? false : true;
+}
