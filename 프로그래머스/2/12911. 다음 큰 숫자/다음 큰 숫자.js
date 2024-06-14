a7634b7d4c46a1c7fe0e6cd6cafcc6b3770e4304
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-14 18:52:03 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,9 +1,3 @@
-function solution(n) {
-    let i = n + 1
-    let nCount = ([...n.toString(2)]).filter(v => v === "1").length
-    
-    while (true) {
-        if (nCount === ([...i.toString(2)]).filter(v => v === "1").length) return i
-        i++
-    }
+function solution(n, a = n+1) {
+    return n.toString(2).match(/1/g).length == a.toString(2).match(/1/g).length ? a : solution(n, a+1);
 }
