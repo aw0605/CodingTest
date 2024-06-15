fce484dfd364dc7e5948f8c840b20f9a06f0bf65
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-15 15:28:57 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,10 +1,7 @@
 function solution(brown, yellow) {
-    for (let i = 1; i <= yellow**0.5; i++){
-        if (yellow % i == 0){
-            let w = (yellow / i) + 2
-            let h = i + 2
-            
-            if (brown == ((w + h - 2) * 2)) return [w, h]
+    for(let i = 0; i <= yellow ** 0.5; i++){
+        if(yellow % i === 0 && yellow/i*2 + i*2 + 4 === brown){
+            return [yellow/i+2, i+2]
         }
     }
 }
