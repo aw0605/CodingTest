8669d91e83f9cf4688e4bb573fe24b500d7eb307
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-16 16:55:53 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,13 +1,4 @@
 function solution(n){
-    let answer = 0;
-    
-    while (n > 0){
-        if (n % 2 == 0) n /= 2
-        else {
-            n--;
-            answer++;
-        }
-    }
-
-    return answer;
+    // 어떤 수를 2로 나누고, 몫을 2로 계속해서 나눠서 나오는 나머지들의 모든 합은 어떤 수를 이진수로 변환한 수의 1의 개수와 같다.
+    return n.toString(2).replace(/0/g, "").length;
 }
