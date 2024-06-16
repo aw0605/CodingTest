a4b9b43a594107bfb2329d204ecbac8beda78822
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-16 12:20:02 +0900

    [level 2] Title: 피보나치 수, Time: 7.47 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,7 +1,7 @@
 def solution(n):
-    answer = [0,1]
+    a, b = 0, 1
     
-    for i in range(2, n+1):
-        answer.append((answer[i-1] + answer[i-2]) % 1234567)
-    
-    return answer[-1]
+    for i in range(n):
+        a, b = b, (a + b) % 1234567
+        
+    return a
