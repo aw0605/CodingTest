f1cdeb449c59de4b2f1e45be826d251b82cd91a5
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-18 14:43:43 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,11 +1,7 @@
-import math
-
 def solution(n,a,b):
     answer = 0
-    
     while a != b:
-        a = math.ceil(a/2)
-        b = math.ceil(b/2)
         answer += 1
+        a, b = (a+1)//2, (b+1)//2
 
     return answer
