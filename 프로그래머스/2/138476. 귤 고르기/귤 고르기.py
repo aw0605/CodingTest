c0788ad6746164b9cf5234940015e311f0d5c752
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-19 01:07:44 +0900

    [level 2] Title: 귤 고르기, Time: 14.40 ms, Memory: 18.1 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,14 +1,12 @@
+import collections
+
 def solution(k, tangerine):
     answer = 0
-    tang_dict = {}
-    for v in tangerine:
-        if v in tang_dict: tang_dict[v] += 1
-        else: tang_dict[v] = 1 
-    tang_dict = sorted(tang_dict.values(), reverse=True)
-    
-    for i in tang_dict:
-        k -= i
+    tang_dict = collections.Counter(tangerine)
+
+    for v in sorted(tang_dict.values(), reverse = True):
+        k -= v
         answer += 1
-        if (k <= 0): break
+        if k <= 0: break
         
     return answer
