5d2645fa6834fdfd5ff7635241c8d1cadefa64e0
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-18 22:59:07 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.10 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,9 +1,5 @@
 def solution(n, words):
-    answers = [words[0]]
-    
     for i in range(1, len(words)):
-        if (words[i] in answers) or (words[i-1][-1] != words[i][0]):
+        if words[i][0] != words[i-1][-1] or words[i] in words[:i]: 
             return [(i%n)+1, (i//n)+1]
-        answers.append(words[i])
-
-    return [0,0]
+    else: return [0,0]
