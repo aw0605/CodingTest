2f919b37376bd3b84d2859f1ad4f43c004e7bb89
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-20 03:44:58 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,25 +1,18 @@
 function solution(s) {
-    if (s.length % 2) return 0;
+    let answer = 0;
     
-    let stack = [] ;
-    let answer = 0; 
-    
-    for (let i = 0; i < s.length; i++){
-        let rotS = s.slice(i) + s.slice(0, i);
-        let flag = true
-        for (let v of rotS) {
-            if (v === "(" || v === "{" || v === "[") stack.push(v)
-            else { 
-                let acc = stack.pop()
-                if(acc === "(" && v === ")") continue;
-                if(acc === "{" && v === "}") continue;
-                if(acc === "[" && v === "]") continue;
-                flag = false;
-                break;
-            }
+    for (let i = 0; i < s.length; i++) {
+        const stack = [];
+        const rotS = s.slice(i) + s.slice(0, i);
+      
+        for (let j of rotS) {
+            if (stack[stack.length - 1] === "(" && j === ")") stack.pop();
+            else if (stack[stack.length - 1] === "[" && j === "]") stack.pop();
+            else if (stack[stack.length - 1] === "{" && j === "}") stack.pop();
+            else stack.push(j);
         }
-        if (flag) answer++;
+        if (!stack.length) answer++;
     }
     
-    return answer; 
+    return answer;
 }
