c4290e2a3e4b4e4277f664821cf41add31104546
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-20 04:16:01 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,17 +1,17 @@
 def solution(s):
     if len(s) % 2: return 0
-
     answer = 0
-    pair = { ")": "(", "}": "{", "]": "[" }
     
     for i in range(len(s)):
         stack = []
-        rotS = s[i:] + s[:i]
+        rots = s[i:] + s[:i]
         
-        for j in rotS:
-            if j in pair and stack and stack[-1] == pair[j]: stack.pop()
+        for j in rots:   
+            if stack and stack[-1] == '[' and j == ']': stack.pop()
+            elif stack and stack[-1] == '{' and j == '}': stack.pop()
+            elif stack and stack[-1] == '(' and j == ')': stack.pop()
             else: stack.append(j)
-
+        
         if not stack: answer += 1
         
     return answer
