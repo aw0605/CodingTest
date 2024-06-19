95da1566dfedc4596e6bada904a10acf6a12aea6
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-20 04:00:02 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,18 +1,19 @@
 function solution(s) {
+    if(s.length % 2) return 0;
+
     let answer = 0;
-    
+    const pair = { "}":"{", "]":"[", ")":"("};
+
     for (let i = 0; i < s.length; i++) {
         const stack = [];
         const rotS = s.slice(i) + s.slice(0, i);
-      
+
         for (let j of rotS) {
-            if (stack[stack.length - 1] === "(" && j === ")") stack.pop();
-            else if (stack[stack.length - 1] === "[" && j === "]") stack.pop();
-            else if (stack[stack.length - 1] === "{" && j === "}") stack.pop();
-            else stack.push(j);
+            if (stack.length && stack[stack.length-1] === pair[j]) stack.pop()
+            else stack.push(j)
         }
         if (!stack.length) answer++;
     }
-    
+
     return answer;
 }
