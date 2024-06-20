c51f809cbdecfda97750968460ec1a3605dc0d5e
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-21 02:44:09 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,9 +1,3 @@
 function solution(citations) {
-    citations.sort((a,b) => b-a)
-    
-    for (let i = 0; i < citations.length; i++){
-        if (citations[i] < i + 1) return i
-    }
-    
-    return citations.length;
+    return citations.sort((a, b) => b - a).filter((v, i) => v >= i + 1).length;
 }
