5bb53d752bd30fc64103d320a9424ee3db358f75
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-20 20:18:10 +0900

    [level 2] Title: 할인 행사, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=javascript) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.02 ms
+메모리: 33.5 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 20일 20:08:50
+2024년 06월 20일 20:18:08
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -0,0 +1,19 @@
+function solution(want, number, discount) {
+    let answer = 0;
+    
+    for (let i = 0; i <= discount.length - 10; i++) {
+        const slice = discount.slice(i, i+10);
+        let flag = true;
+        
+        for (let j = 0; j < want.length; j++) {
+            if (slice.filter(v => v === want[j]).length !== number[j]){
+                flag = false;
+                break;
+            }
+        }
+        
+        if (flag) answer++;
+    }
+    
+    return answer;
+}
