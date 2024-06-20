c7a14852b54956642671280a5ba6980b8f409ab6
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-20 20:08:51 +0900

    [level 2] Title: 할인 행사, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.03 ms
+메모리: 10.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 20일 19:51:21
+2024년 06월 20일 20:08:50
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,17 +1,10 @@
 from collections import Counter
 
 def solution(want, number, discount):
-    item_dict = {v: n for v, n in zip(want, number)}
-    total = sum(number)
     answer = 0
-    
-    for i in range(len(discount) - total+1):
-        sale_dict = Counter(discount[i:i+total])
-        
-        for v in want:
-            if item_dict[v] == sale_dict[v]: sale_dict[v] = 0
-            else: break
+    item_dict = {v: n for v,n in zip(want, number)}
 
-        if sum(sale_dict.values()) == 0: answer += 1 
-    
+    for i in range(len(discount)-sum(number)+1):
+        if item_dict == Counter(discount[i:i+sum(number)]): answer += 1
+
     return answer
