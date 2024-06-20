1a60d2195add4627e86cc923d6c37897c33865f7
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-20 20:34:53 +0900

    [level 2] Title: 할인 행사, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,19 +1,20 @@
 function solution(want, number, discount) {
     let answer = 0;
+    let item_dict = {};
+    for (let i = 0; i < number.length; i++) item_dict[want[i]] = number[i];
     
-    for (let i = 0; i <= discount.length - 10; i++) {
-        const slice = discount.slice(i, i+10);
+    for (let i = 0; i <= discount.length-10; i++) {
+        let slice = discount.slice(i, i+10);
         let flag = true;
         
-        for (let j = 0; j < want.length; j++) {
-            if (slice.filter(v => v === want[j]).length !== number[j]){
+        for (let j of want) {
+            if (slice.filter((v) => v === j).length !== item_dict[j]) {
                 flag = false;
                 break;
             }
         }
-        
         if (flag) answer++;
     }
     
-    return answer;
+  return answer;
 }
