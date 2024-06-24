451b13f2b5715bc24ed727d59be6ff837ed9c63c
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-24 18:28:28 +0900

    [level 2] Title: 행렬의 곱셈, Time: 4.79 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,2 +1,4 @@
 def solution(arr1, arr2):
-     return [[sum(a*b for a, b in zip(A1,A2)) for A2 in zip(*arr2)] for A1 in arr1]
+    return [[sum(a*b for a, b in zip(A1,A2)) for A2 in zip(*arr2)] for A1 in arr1]
+    # for A2 in zip(*arr2): 풀어서 행의 요소를 열별로 그룹화
+    # [[5, 4, 3], [2, 4, 1], [3, 1, 1]] => [[5,2,3],[4,4,1],[3,1,1]]
