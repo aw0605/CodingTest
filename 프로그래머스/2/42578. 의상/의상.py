77ffc6fb1d83e2b75f2669ac2df04a30b084f478
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-24 19:08:52 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,12 +1,7 @@
+from collections import Counter
+from functools import reduce
+
 def solution(clothes):
-    answer = 1
-    
-    clo_dict = {}
-    for v in clothes:
-        if v[1] in clo_dict: clo_dict[v[1]].append(v[0])
-        else: clo_dict[v[1]] = [v[0]]
-        
-    for items in clo_dict.values():
-        answer *= (len(items) + 1)
-    
-    return answer - 1
+    cnt = Counter([key for _, key in clothes])
+    answer = reduce(lambda x, y: x * (y+1), cnt.values(), 1) - 1
+    return answer
