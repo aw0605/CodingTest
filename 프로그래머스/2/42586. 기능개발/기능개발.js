3b46462c13eb1e690579b45c06b608c6e41241f3
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-06-26 18:44:19 +0900

    [level 2] Title: 기능개발, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,18 +1,19 @@
 function solution(progresses, speeds) {
-    let answer = [0];
-    let rest = []
-    for (let i = 0; i < speeds.length; i++){
-        rest.push(Math.ceil((100 - progresses[i]) / speeds[i]))
-    }
-    
-    let max = rest[0];
+    let answer = [];
 
-    for (let i = 0, j = 0; i< rest.length; i++){
-        if(rest[i] <= max) answer[j] += 1;
-        else {
-            max = rest[i];
-            answer[++j] = 1;
+    while (speeds.length > 0) {
+        for (let i in speeds) {
+            if (progresses[i] < 100) progresses[i] += speeds[i];
         }
+
+        let d = 0;
+        while (progresses[0] >= 100) {
+            progresses.shift();
+            speeds.shift();
+            d++;
+        }
+        
+        if (d > 0) answer.push(d);
     }
 
     return answer;
