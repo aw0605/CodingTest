c3caa75e66bdc014cbe141c3a14c18083cb36d06
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-04 22:34:55 +0900

    [level 2] Title: [1차] 캐시, Time: 0.69 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,20 +1,17 @@
+import collections
+
 def solution(cacheSize, cities):
-    cache = []
+    cache = collections.deque(maxlen=cacheSize)
     time = 0
     
     for city in cities:
         city = city.lower()
-        if cacheSize:
-            # cache miss: 오래된 값 제거 후, 새로운 값 cache에 저장
-            if not city in cache:
-                if len(cache) == cacheSize: cache.pop(0)
-                cache.append(city)
-                time += 5
-            # cache hit: cache에서 기존 값 제거 후, 맨 뒤에 다시 저장
-            else:
-                cache.pop(cache.index(city))
-                cache.append(city)
-                time += 1
-        else: time += 5
-        
+        if city in cache:
+            cache.remove(city)
+            cache.append(city)
+            time += 1
+        else:
+            cache.append(city)
+            time += 5
+            
     return time
