49e418de0604a7d325b2ce2b93cd484a5358bb07
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-05 16:51:35 +0900

    [level 2] Title: 타겟 넘버, Time: 3.86 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,5 @@
 def solution(numbers, target):
-    answer = 0
-    queue = [[numbers[0],0], [-1 * numbers[0],0]]
-    n = len(numbers)
-
-    while queue:
-        cur, i = queue.pop()
-        i += 1
-        if i < n:
-            queue.append([cur+numbers[i], i])
-            queue.append([cur-numbers[i], i])
-        else:
-            if cur == target: answer += 1
-            
-    return answer
+    if not numbers and target == 0: return 1
+    elif not numbers: return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
