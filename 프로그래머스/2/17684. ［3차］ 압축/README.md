6dbbd49d1faa488904ab1d3a1c19e8b1974de752
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-08 21:50:13 +0900

    [level 2] Title: [3차] 압축, Time: 121.80 ms, Memory: 50.2 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/README.md
+++ 프로그래머스/2/17684. ［3차］ 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 압축 - 17684 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.46 ms
+메모리: 50.2 MB, 시간: 121.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 08일 19:49:42
+2024년 07월 08일 21:50:09
 
 ### 문제 설명
 
--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,28 +1,18 @@
 function solution(msg) {
     let answer = [];
-    let dict = {}
-    const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    for (let i = 0; i < 26; i++) dict[alpha[i]] = i+1
+    const dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('').reduce((dict, v, i) => {
+        dict[v] = i+1;
+        return dict;
+    }, {});
+    dict["nextId"] = 27;
     
-    let nextIndex = 27;
-    let i = 0;
-    
-    while (i < msg.length) {
-        let w = msg[i];
-        let j = i + 1;
-        
-        // dict에 있는 wc 중 가장 긴 문자열 찾기
-        while (j <= msg.length && dict[msg.slice(i, j)]) {
-            w = msg.slice(i, j);
-            j++;
-        }
-        // wc가 있다면 slice한 w의, 없다면 idx의 w의 value를 answer에 추가
-        answer.push(dict[w]);
-        // wc가 없다면 dict 업데이트
-        if (j <= msg.length) dict[msg.slice(i, j)] = nextIndex++;
-        // 다음에 찾을 문자로 이동
-        i += w.length;
+    for (let i = 0, j = 0; i < msg.length; i = j){
+        j = msg.length;
+        let longest = '';
+        while (dict[(longest = msg.slice(i, j))] === undefined) --j;
+        answer.push(dict[longest]);
+        dict[longest + msg[j]] = dict["nextId"]++;
     }
-    
+
     return answer;
 }
