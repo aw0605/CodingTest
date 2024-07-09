eb937d291546d9534e4236b8cbd2b9373f9e370f
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-09 18:09:08 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,10 +1,5 @@
-from itertools import product
-
 def solution(word):
     answer = 0
-    words = ["A", "E", "I", "O", "U"]
-    dictWords = []
-    for i in range(1,6):
-        dictWords.extend([''.join(v) for v in product(words, repeat=i)])
-    
-    return sorted(dictWords).index(word) + 1
+    for i, n in enumerate(word):
+        answer += (5 ** (5 - i) - 1) / (5 - 1) * "AEIOU".index(n) + 1
+    return answer
