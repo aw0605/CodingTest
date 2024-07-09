dfb1d566dc4bd72947021bbc7fae8488d2f934bf
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-09 18:16:43 +0900

    [level 2] Title: 모음 사전, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,14 +1,4 @@
 function solution(word) {
-    const answer = {};
-    let n = 0;
-    function dfs(w) {
-        if (w.length > 5) return;
-        answer[w] = n;
-        n++;
-        for (let v of "AEIOU") {
-            dfs(w + v);
-        }
-    }
-    dfs("");
-    return answer[word];
+    return word.split('')
+            .reduce((a,c,i) => a + ((5**(5-i)-1) / 4) * ('AEIOU'.indexOf(c)) + 1, 0)
 }
