6efedfb9c9bbc82fb7a18dbc936a0c69d66c5f1b
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-12 21:39:05 +0900

    [level 2] Title: 더 맵게, Time: 486.03 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=javascript) 
 
 ### 성능 요약
 
-메모리: 67 MB, 시간: 1195.01 ms
+메모리: 113 MB, 시간: 486.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 12일 18:14:56
+2024년 07월 12일 21:39:00
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -0,0 +1,62 @@
+function solution(scoville, K) {
+    scoville.sort((a,b)=> b - a) 
+    let answer = 0;
+    let under_K = []
+    let flag = 0
+
+    for (let i = 0; i < scoville.length; ++i) {
+        if (scoville[i] < K) under_K.push(scoville[i])
+        else flag = 1
+    }    
+
+    let mixed = []
+    let m_i = 0
+    let i = 0
+    let n1 = 0 
+    let n2 = 0
+    while (1){
+        if (mixed[m_i] != undefined) {
+            if (under_K.length != 0) {
+                if (under_K.at(-1) < mixed[m_i]) n1 = under_K.pop()
+                else {
+                    n1 = mixed[m_i]
+                    m_i++
+                }
+            }
+            else {
+                n1 = mixed[m_i]
+                m_i++
+            }
+        }
+        else {
+            if (under_K.length != 0) n1 = under_K.pop()
+            else break;
+        }
+        if (mixed[m_i] != undefined) {
+            if (under_K.length != 0) {
+                if (under_K.at(-1) < mixed[m_i]) n2 = under_K.pop()
+                else {
+                    n2 = mixed[m_i]
+                    m_i++
+                }
+            }
+            else {
+                n2 = mixed[m_i]
+                m_i++
+            }
+        }
+        else {
+            if (under_K.length != 0) n2 = under_K.pop()
+            else {
+                answer++
+                break;
+            }
+        }
+
+        if (n1+n2*2 < K) mixed.push(n1+n2*2)
+        else flag = 1 
+        answer++
+    }
+
+    return flag == 1 ? answer : -1
+}
