f2000a375efff7ca16300123816452a785fa4498
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-16 01:27:08 +0900

    [level 2] Title: 스킬트리, Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -1,14 +1,7 @@
 function solution(skill, skill_trees) {
-    let answer = 0;
-    
-    for (let v of skill_trees){
-        let order = ""
-        for (let s of v){
-            if (skill.includes(s)) order += s
-        }
-        
-        if (skill.slice(0, order.length) === order) answer++
-    }
-    
-    return answer;
+    const regex = new RegExp(`[^${skill}]`, 'g');
+
+    return skill_trees
+        .map((v) => v.replace(regex, ''))
+        .filter((s) => {return skill.startsWith(s);}).length
 }
