fdd9107d5b8c9651e4c1e8db2ab1d8a0fb2ca3aa
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-15 21:34:38 +0900

    [level 2] Title: 스킬트리, Time: 0.02 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,11 +1,12 @@
 def solution(skill, skill_trees):
     answer = 0
-    
+
     for v in skill_trees:
-        order = ""
-        for s in v:       
-            if s in skill: order += s
-        
-        if skill[:len(order)] == order: answer += 1
-    
+        skillArr = list(skill)
+
+        for s in v:
+            if s in skill:
+                if s != skillArr.pop(0): break
+        else: answer += 1
+
     return answer
