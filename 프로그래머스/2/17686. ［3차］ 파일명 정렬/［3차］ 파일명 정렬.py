530126d3bf7b5ed16b44ef0bb5cdfaa782c662ca
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-18 20:35:44 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 3.67 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,15 +1,9 @@
 import re
 
 def solution(files):
+
     def split_file(file):
-        original = file
-        file = file.lower()
-        num = re.search(r'[0-9]+', file).group()
-        head, tail = re.split(num, file, maxsplit=1)
-        return [head, int(num), original]
+        head,num,tail = re.match(r'([a-zA-Z-. ]+)(\d{,5})(.*)',file).groups()
+        return [head,int(num)]
 
-    fileInfo = [split_file(file) for file in files]
-    
-    fileInfo.sort(key = lambda x: (x[0], x[1]))
-
-    return [file[2] for file in fileInfo]
+    return sorted(files, key = lambda x: split_file(x.lower()))
