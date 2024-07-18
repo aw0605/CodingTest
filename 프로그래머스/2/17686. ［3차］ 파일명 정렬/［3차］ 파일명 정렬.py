38e52287b986067b55013de329c1105724be1c32
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-18 20:41:27 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 4.82 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,6 +1,18 @@
-import re
+def split_file(files):    
+    route = 0
+    answer = ['', '']
+    for i, v in enumerate(files):
+        if (route == 0):
+            if (v.isnumeric() == True):
+                answer[0] = files[:i].lower()
+                route = 1
+                answer[1] += v
+        elif (route == 1):
+            if (v.isnumeric() == True): answer[1] += v
+            else: break
+    answer[1] = int(answer[1])
+    return answer
+
 
 def solution(files):
-    a = sorted(files, key=lambda file : int(re.findall('\d+', file)[0]))
-    b = sorted(a, key=lambda file : re.split('\d+', file.lower())[0])
-    return b
+    return sorted(files, key = lambda x:split_file(x))
