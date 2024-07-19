186b4292804a6a14d06af82e6e99e5933422aa8f
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-19 21:51:51 +0900

    [level 2] Title: 오픈채팅방, Time: 198.12 ms, Memory: 42.4 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,14 +1,4 @@
 def solution(record):
-    answer = []
-    user = {}
+    user = {v.split()[1]:v.split()[-1] for v in record if v.startswith('Enter') or v.startswith('Change')}
     
-    for v in record:
-        v = v.split()
-        if v[0] in ["Enter", "Change"]: user[v[1]] = v[2]
-    
-    for v in record:
-        v = v.split()
-        if v[0] == "Enter": answer.append(f'{user[v[1]]}님이 들어왔습니다.')
-        elif v[0] == "Leave": answer.append(f'{user[v[1]]}님이 나갔습니다.')
-    
-    return answer
+    return [f'{user[v.split()[1]]}님이 들어왔습니다.' if v.startswith('Enter') else f'{user[v.split()[1]]}님이 나갔습니다.' for v in record if not v.startswith('Change')]
