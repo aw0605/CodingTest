5836cdb37eef06c986e52ef175cd6d96c2fbcc89
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-21 21:59:28 +0900

    [level 2] Title: 숫자 변환하기, Time: 386.14 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,17 +1,17 @@
-from collections import deque
-
 def solution(x, y, n):
-    visited = [0] * 1000001
-    q = deque()
-    visited[x] = 1
-    q.append((x, 0))
-    
-    while q:
-        num, cnt = q.popleft()
-        if num == y: return cnt
-        for next_num in (num + n, num * 2, num * 3):
-            if next_num <= y and not visited[next_num]:
-                visited[next_num] = 1
-                q.append((next_num, cnt + 1))
+    answer = 0
+    s = set()
+    s.add(x)
+
+    while s:
+        if y in s: return answer
+        nxt = set()
+        for i in s:
+            if i+n <= y: nxt.add(i+n)
+            if i*2 <= y: nxt.add(i*2)
+            if i*3 <= y:nxt.add(i*3)
+            
+        s = nxt
+        answer += 1
 
     return -1
