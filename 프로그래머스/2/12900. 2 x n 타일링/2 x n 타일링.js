7013f414fa6f5edeef5c07d6cb58868799416b7f
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-22 17:45:33 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.43 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -1,8 +1,14 @@
 function solution(n) {
-    const dp = [1, 2];
-    
+    if (n === 1) return 1;
+    if (n === 2) return 2;
+
+    const dp = Array(n).fill(0);
+    dp[0] = 1;
+    dp[1] = 2;
+
     for (let i = 2; i < n; i++) {
-        dp[i] = (dp[i - 2] + dp[i - 1]) % 1000000007;
+        dp[i] = (dp[i-2] + dp[i-1]) % 1000000007;
     }
+
     return dp[n-1];
 }
