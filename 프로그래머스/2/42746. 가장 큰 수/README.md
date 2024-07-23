216062b4bd77850fc813d7e62beff65a71364dc2
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-23 14:04:11 +0900

    [level 2] Title: 가장 큰 수, Time: 2268.37 ms, Memory: 24.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=python3) 
 
 ### 성능 요약
 
-메모리: 27.1 MB, 시간: 1370.48 ms
+메모리: 24.9 MB, 시간: 2268.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 23일 13:49:17
+2024년 07월 23일 14:04:09
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,5 +1,11 @@
-def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key = lambda x: x * 3, reverse=True)
+import functools
 
-    return str(int(''.join(numbers)))
+def compare(a,b):
+    c1 = a+b
+    c2 = b+a
+    return (int(c1) > int(c2)) - (int(c1) < int(c2))
+
+def solution(numbers):
+    n = [str(x) for x in numbers]
+    n = sorted(n, key=functools.cmp_to_key(compare),reverse=True)
+    return str(int(''.join(n)))
