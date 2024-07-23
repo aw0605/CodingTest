0b8895c23d56afc0a4a4b4ab5aca06a693a7cedc
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-23 18:43:27 +0900

    [level 2] Title: 가장 큰 수, Time: 301.91 ms, Memory: 45.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,4 +1,4 @@
 function solution(numbers) {
-    let answer = numbers.sort((a, b) => `${b}${a}` - `${a}${b}`).join('');
+    let answer = numbers.map(v => v + '').sort((a,b) => +(b+a) - +(a+b)).join('');
     return answer[0] === '0' ? '0' : answer;
 }
