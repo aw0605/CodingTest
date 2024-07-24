974f6bd34d961dae7cba8146ffaec52d688b70af
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-24 20:54:08 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 501.37 ms, Memory: 23.8 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -1,14 +1,21 @@
-def solution(numbers):
-    answer = []
-    for number in numbers:
-        if number & 1:
-            target = number
-            idx = 0
-            while number > 0:
-                if number % 2 == 0: break
-                number //= 2
-                idx += 1
-            answer.append(target + 2 ** (idx) - 2 ** (idx-1))
-        else: answer.append(number+1)
+def f(number):
+    bin_num = bin(number)[2:]
 
+    if '0' not in bin_num:
+        return int('10' + bin_num[1:], 2)
+
+    bin_num = list(bin_num)
+    for i in range(len(bin_num)):
+        if bin_num[-i-1] == '0':
+            bin_num[-i-1] = '1'
+            break
+
+    if i > 0:
+        bin_num[-i] = '0'
+
+    return int(''.join(bin_num), 2)
+
+
+def solution(numbers):
+    answer = [f(number) for number in numbers]
     return answer
