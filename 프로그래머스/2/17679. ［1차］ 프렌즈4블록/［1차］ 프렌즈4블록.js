21a4548316dee7df8132ef69b707a48ed0342288
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-25 18:20:47 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 49.41 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,51 +1,36 @@
 function solution(m, n, board) {
-  let answer = 0;
-  board = board.map(v => v.split(''));
-  let tmp = new Set();
-
-  while (true) {
-    for (let i = 0; i < m - 1; i++) {
-      for (let j = 0; j < n - 1; j++) {
-        let c = board[i][j];
-        if (c === '') continue;
-
-        if (
-          board[i][j + 1] === c &&
-          board[i + 1][j] === c &&
-          board[i + 1][j + 1] === c
-        ) {
-          tmp.add(`${i},${j}`);
-          tmp.add(`${i},${j + 1}`);
-          tmp.add(`${i + 1},${j}`);
-          tmp.add(`${i + 1},${j + 1}`);
-        }
-      }
-    }
-
-    if (tmp.size) {
-      answer += tmp.size;
-      tmp.forEach(position => {
-        const [i, j] = position.split(',').map(Number);
-        board[i][j] = '';
-      });
-      tmp.clear();
-    } else break;
+    board = board.map(v => v.split(''));
 
     while (true) {
-      let moved = false;
-      for (let i = m - 2; i >= 0; i--) {
-        for (let j = 0; j < n; j++) {
-          if (board[i][j] !== '' && board[i + 1][j] === '') {
-            board[i + 1][j] = board[i][j];
-            board[i][j] = '';
-            moved = true;
-          }
-        }
-      }
-      if (!moved) break;
-    }
-  }
+        let founded = [];
 
-  return answer;
+        for (let i = 1; i < m; i++) {
+            for (let j = 1; j < n; j++) {
+                if (board[i][j] && board[i][j] === board[i][j - 1] && board[i][j] === board[i - 1][j - 1] && board[i][j] === board[i - 1][j]) founded.push([i, j]);
+            }
+        }
+
+        if (!founded.length) return [].concat(...board).filter(v => ! v).length;
+
+        founded.forEach(a => {
+            board[a[0]][a[1]] = 0;
+            board[a[0]][a[1] - 1] = 0;
+            board[a[0] - 1][a[1] - 1] = 0;
+            board[a[0] - 1][a[1]] = 0;
+        });
+
+        for (let i = m - 1; i > 0; i--) {
+            if (!board[i].some(v => ! v)) continue;
+
+            for (let j = 0; j < n; j++) {
+                for (let k = i - 1; k >= 0 && ! board[i][j]; k--) {
+                    if (board[k][j]) {
+                        board[i][j] = board[k][j];
+                        board[k][j] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
-     
