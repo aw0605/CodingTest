b1f54306603ea9d67b326db1067a6ea819d7b348
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-26 23:05:38 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 53.10 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,31 +1,16 @@
 function solution(arr) {
-    var answer = [0,0];
-    
-    const divide = (row, col, len) => {
-        
-    let dividable = true;
-    
-    for(let i = row; i < row + len; i++) { 
-        for(let j = col; j < col + len; j++) {
-            if(arr[row][col] !== arr[i][j]) {
-                dividable = false;
-            }
+    const quadZip = (row, col, len) => {
+        if (len < 2) return arr[row][col] ? [0, 1] : [1, 0];
+        let cnt0 = 0, cnt1 = 0; len /= 2;
+        for (let [i, j] of [[0,0],[0,1],[1,0],[1,1]]) {
+            let [zero, one] = quadZip(row+i*len, col+j*len, len);
+            cnt0 += zero;
+            cnt1 += one;
         }
+        if (cnt0 === 0) return [0, 1];
+        if (cnt1 === 0) return [1, 0];
+        return [cnt0, cnt1];
     }
     
-    let half = Math.floor(len / 2);
-        
-    if(!dividable) {
-        divide(row, col, half)
-        divide(row + half, col, half)
-        divide(row, col + half, half)
-        divide(row + half, col + half, half)
-    }else {
-        if(arr[row][col] === 1) answer[1]++;
-        else answer[0]++;
-    }
-}
-    
-    divide(0, 0, arr.length);
-    return answer;
+    return quadZip(0, 0, arr.length);
 }
