f950fe6dc321a2487323cc81345b9c384900df52
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-30 23:52:24 +0900

    [level 2] Title: 소수 찾기, Time: 2106.43 ms, Memory: 175 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,10 +1,56 @@
-from itertools import permutations
+def isPrimeNumber(number):
+    if number <= 1: return False
+    for i in range(2, number // 2 + 1):
+        if number % i == 0:return False
+    return True
 
-def solution(n):
-    answer = set()
-    for i in range(len(n)):
-        answer |= set(map(int, map("".join, permutations(list(n), i + 1))))
-    answer -= set(range(0, 2))
-    for i in range(2, int(max(answer) ** 0.5) + 1):
-        answer -= set(range(i * 2, max(answer) + 1, i))
-    return len(answer)
+def getAllCombination(numbers, numList, leftCipher):
+    newNumList = [[]]
+    for li in numList:
+        for i in numbers:
+            if i in li and li.count(i) <= numbers.count(i) - 1:
+                tmp = li[:]
+                tmp.append(i)
+                newNumList.append(tmp)
+            if i not in li:
+                tmp = li[:]
+                tmp.append(i)
+                newNumList.append(tmp)
+
+    leftCipher -= 1
+
+    if leftCipher == 0: return newNumList
+    else: return getAllCombination(numbers, newNumList, leftCipher)
+
+def removeFirstZero(numList):
+    for i, num in enumerate(numList):
+        firstNumIsZero = bool()
+
+        while True:
+            if len(num) >= 2 and num[0] == '0': firstNumIsZero = True
+            else:
+                numList[i] = num
+                break
+
+            num = num[1:]
+
+def getUnique2DList(numList):
+    for i, val in enumerate(numList):
+        tmp = str()
+        for char in val: tmp += char
+        numList[i] = tmp
+
+    newList = list(set(numList))
+    return newList
+
+def solution(numbers):    
+    availableAnswer = getAllCombination(numbers, [[]], len(numbers))
+    del availableAnswer[0]
+    removeFirstZero(availableAnswer)
+    availableAnswer = getUnique2DList(availableAnswer)
+
+    answer = 0
+    for val in availableAnswer:
+        if isPrimeNumber(int(val)): answer += 1
+
+    return answer
