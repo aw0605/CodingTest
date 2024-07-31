025ba681c0d1c3992ab7614dc836336ee7f3b929
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-31 17:50:56 +0900

    [level 2] Title: 삼각 달팽이, Time: 148.81 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -1,16 +1,17 @@
-def solution(n):
-    tri = [[0] * i for i in range(1,n+1)]
-    x,y = -1,0
-    num = 1
+from itertools import chain
 
+def solution(n):
+    [x, y, num] = [-1, 0, 1]
+    board = [[None] * i for i in range(1, n+1)]
+    
     for i in range(n):
-        for j in range(i,n):
+        for _ in range(n-i):
             if i % 3 == 0: x += 1
             elif i % 3 == 1: y += 1
             else:
                 x -= 1
                 y -= 1
-            tri[x][y] = num
+            board[x][y] = num
             num += 1
             
-    return sum(tri, [])
+    return list(chain(*board))
