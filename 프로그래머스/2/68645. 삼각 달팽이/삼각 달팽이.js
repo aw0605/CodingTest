6754f27d9c9ceb4ccbbabaad704db4fad14bfc55
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-07-31 18:10:53 +0900

    [level 2] Title: 삼각 달팽이, Time: 84.70 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -1,20 +1,13 @@
 function solution(n) {
-    const tri = Array.from({ length: n }, (_, i) => Array(i + 1).fill(0));
-    let x = -1, y = 0;
-    let num = 1;
-
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n - i; j++) {
-            if (i % 3 === 0) x++;
-            else if (i % 3 === 1) y++;
-            else {
-                x--;
-                y--;
-            }
-            tri[x][y] = num;
-            num++;
-        }
-    }
-
-    return tri.flat();
+  let tri = Array(n).fill().map((_, i) => Array(i + 1).fill())
+  let x = -1, y = 0
+  let num = 0
+  
+  for (let i = n; i > 0; i -= 3) {
+    tri[++x][y] = ++num
+    for (let j = 0; j < i - 1; j++) tri[++x][y] = ++num
+    for (let j = 0; j < i - 1; j++) tri[x][++y] = ++num
+    for (let j = 0; j < i - 2; j++) tri[--x][--y] = ++num
+  }
+  return tri.flat()
 }
