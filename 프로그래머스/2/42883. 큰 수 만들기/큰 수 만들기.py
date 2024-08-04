7741f7a6c9c0ee99a2b44b227800f543beed2dc0
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-04 17:18:56 +0900

    [level 2] Title: 큰 수 만들기, Time: 89.85 ms, Memory: 13 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,12 @@
+def solution(number, k):
+    answer = []
+    
+    for n in number:
+        while k > 0 and answer and answer[-1] < n:
+            answer.pop()
+            k -= 1
+        answer.append(n)
+    
+    if k != 0: answer = answer[:-k]
+    
+    return ''.join(answer)
