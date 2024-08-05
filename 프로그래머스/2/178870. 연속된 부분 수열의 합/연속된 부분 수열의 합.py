41ffbb366ca43807ba6d6f0e2adbb2fd4c50fa2e
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-05 19:03:01 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 492.06 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,15 +1,18 @@
 def solution(sequence, k):
-    dic = {i:v for i,v in enumerate(sequence)}
     answer = []
-    start, sum = 0,0
+    s, e = 0, 0
+    total = sequence[0]
+    min_len = 2147483647
+    
+    while s <= e < len(sequence):
+        if total >= k:
+            if total == k and min_len > e - s + 1:
+                min_len = e - s + 1
+                answer = [s, e]
+            total -= sequence[s]
+            s += 1
+        else:
+            e += 1
+            if e < len(sequence): total += sequence[e]
 
-    for i,v in enumerate(sequence):
-        end = i
-        sum += v
-        while sum > k :
-            sum -= dic[start]
-            start += 1
-        if sum == k: answer.append([start,end])
-
-    answer.sort(key = lambda x: (x[1]-x[0],x[0]))
-    return answer[0]
+    return answer
