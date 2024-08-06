8fc3fe4d96d8ced6066f570a0ceff01d00158c9e
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-06 17:26:43 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -1,5 +1,17 @@
 function solution(storey) {
-    if (storey < 10) return Math.min(storey, 11 - storey)
-    let left = storey % 10
-    return Math.min(left + solution(Math.trunc(storey/10)), 10 - left + solution(Math.trunc(storey/10)+1));
+    let answer = 0
+    const x = String(storey).split("").map(a => Number(a))
+
+    for(let i = x.length-1; i >= 0; i--) {
+        if (x[i] > 5) {
+            answer += 10 - x[i]
+            if (i === 0) answer++
+            x[i-1]++
+        } else if(x[i] === 5 && i > 0 && x[i-1] >= 5) {
+            answer+= 5
+            x[i-1]++    
+        } else answer += x[i]
+    }
+    
+    return answer
 }
