7dd187131a166c48b6703e200d3e734ca08ce682
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-06 17:14:59 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -1,15 +1,4 @@
 def solution(storey):
-    answer = 0
-    x = list(map(int, str(storey)[::-1]))
-
-    for i in range(len(x)):
-        if x[i] < 5: answer += x[i]
-        elif x[i] == 5:
-            answer += 5
-            if i+1 < len(x) and x[i+1] >= 5: x[i+1] += 1 
-        else: 
-            answer += 10 - x[i]
-            if i+1 < len(x): x[i+1] += 1
-            else: answer += 1
-
-    return answer
+    if storey < 10 : return min(storey, 11 - storey)
+    left = storey % 10
+    return min(left + solution(storey // 10), 10 - left + solution(storey // 10 + 1))
