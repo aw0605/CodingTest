e8e021e5a2d9c4b27d0ee98321babd32bb75ed80
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-07 16:54:03 +0900

    [level 2] Title: 시소 짝꿍, Time: 14.27 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -1,13 +1,17 @@
-from collections import Counter
-
 def solution(weights):
-    counter = Counter(weights)
     answer = 0
+    dic = {}
+    friend_list = [2,3/2,4/3]
+    
+    for weight in weights:
+        if weight in dic: dic[weight] += 1
+        else: dic[weight] = 1
 
-    for c in counter:
-        answer += counter[c] * (counter[c] - 1) // 2 
-        answer += counter[c] * counter[c * 4 / 3] 
-        answer += counter[c] * counter[c * 4 / 2] 
-        answer += counter[c] * counter[c * 3 / 2] 
+    for weight in dic:
+        if dic[weight]>1:
+            answer += dic[weight] * (dic[weight]-1)/2
+        for friend in friend_list:
+            if weight * friend in dic:
+                answer += dic[weight] * dic[weight*friend]
 
     return answer
