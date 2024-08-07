182e03c2e57f69f9d61444bc464f1bfe6fd5e94d
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-07 17:28:21 +0900

    [level 2] Title: 시소 짝꿍, Time: 8.82 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,14 +1,14 @@
 function solution(weights) {
-  weights.sort((a, b) => b - a);
-  const dict = {};
+    let answer = 0;
+    const dict = new Map();
+    for (const weight of weights) dict.set(weight, (dict.get(weight) || 0) + 1);
 
-  return weights.reduce((answer, w) => {
-    if (dict[w]) answer += dict[w];
-    if (dict[(w * 4) / 3]) answer += dict[(w * 4) / 3];
-    if (dict[(w * 3) / 2]) answer += dict[(w * 3) / 2];
-    if (dict[w * 2]) answer += dict[w * 2];
+    for (const [weight, count] of dict.entries()) {
+        answer += (count * (count - 1)) / 2;
+        answer += count * (dict.get(weight * 4 / 3) || 0);
+        answer += count * (dict.get(weight * 4 / 2) || 0);
+        answer += count * (dict.get(weight * 3 / 2) || 0);
+    }
 
-    dict[w] = (dict[w] || 0) + 1;
     return answer;
-  }, 0);
 }
