fe36bb0b6e2d9ace40eea7e88c471b6b91fec36f
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-13 18:28:34 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 93.79 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,34 +1,15 @@
-import sys
-
-def dict_update(d, key):
-    if key in d: d[key] += 1
-    else: d[key] = 1
-
-def order_update_rec(d, s, i, rst):
-    if i == len(s): dict_update(d, rst)
-    else:
-        order_update_rec(d, s, i+1, rst)
-        order_update_rec(d, s, i+1, rst+s[i])
+from itertools import combinations
+from collections import Counter
 
 def solution(orders, course):
-    order_dict = {}
-    for order_i in range(len(orders)):
-        orders[order_i] = sorted(orders[order_i])
-        order_update_rec(order_dict, orders[order_i], 0, "")
+    answer = []
+    for c in course:
+        temp = []
+        for order in orders:
+            combi = combinations(sorted(order), c)
+            temp += combi
+        counter = Counter(temp)
+        if len(counter) != 0 and max(counter.values()) != 1:
+            answer += [''.join(f) for f in counter if counter[f] == max(counter.values())]
 
-    result = []
-    max_v = [ 0 for _ in range(len(course)) ]
-    max_key = [ [] for _ in range(len(course)) ]
-    for key in order_dict:
-        for course_i, course_n in enumerate(course):
-            if len(key) == course_n:
-                if order_dict[key] > max_v[course_i]:
-                    max_v[course_i] = order_dict[key]
-                    max_key[course_i] = [key]
-                elif order_dict[key] == max_v[course_i]:
-                    max_key[course_i].append(key)
-    for i, v in enumerate(max_v):
-        if v >= 2:
-            result += max_key[i]
-
-    return sorted(result)
+    return sorted(answer)
