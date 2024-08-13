471b1620408152f6c2e98f2bac0f29732ebb9474
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-13 18:23:53 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 6.01 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -1,39 +1,31 @@
 function solution(orders, course) {
-	const result = [];
-    const map = new Map();
-    const combinations = [];
+  const ordered = {};
+  const candidates = {};
+  const maxNum = Array(10 + 1).fill(0);
     
-    const combination = (menus, pick) => {
-        if (pick.length >= 2) combinations.push(pick);
-        if (!menus.length) return;
-        
-        for (let i = 0; i < menus.length; i++) {
-            const picked = menus[i];
-            const deletedMenu = menus.slice(i + 1);
-            combination(deletedMenu, pick + picked);
-        }
-    };
+  const createSet = (arr, start, len, foods) => {
+    if (len === 0) {
+      ordered[foods] = (ordered[foods] || 0) + 1;
+      if (ordered[foods] > 1) candidates[foods] = ordered[foods];
+      maxNum[foods.length] = Math.max(maxNum[foods.length], ordered[foods]);
+      return;
+    }
 
-    orders.forEach(order => combination([...order].sort().join(''), ''));
-    
-    combinations.forEach(menu => {
-        const isExist = map.has(menu);
-        map.set(menu, isExist ? map.get(menu) + 1 : 1);
+    for (let i = start; i < arr.length; i++) {
+      createSet(arr, i + 1, len - 1, foods + arr[i]);
+    }
+  };
+
+  orders.forEach((od) => {
+    const sorted = od.split('').sort();
+    course.forEach((len) => {
+      createSet(sorted, 0, len, '');
     });
-    
-    const menuArr = [...map.entries()].sort(([_, firstCount], [__, secondCount]) => secondCount - firstCount).filter(([_, menuCount]) => menuCount !== 1);
-    
-    course.forEach(menuLength => {
-        let countMemo = 0;
-        for (i = 0; i < menuArr.length; i++) {
-            const [menu, count] = menuArr[i];
-            if (countMemo > count) break;
-            if (menu.length === menuLength) {
-                if (!countMemo) countMemo = count;
-                result.push(menu);
-            }
-        } 
-    });
-    
-    return result.sort();
+  });
+
+  const launched = Object.keys(candidates).filter(
+    (food) => maxNum[food.length] === candidates[food]
+  );
+
+  return launched.sort();
 }
