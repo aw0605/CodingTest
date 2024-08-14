7482c57c7dd05bd4064ab4bed651fe641a2a2157
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-14 21:21:52 +0900

    [level 2] Title: [3차] 방금그곡, Time: 1.67 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -1,23 +1,17 @@
-def time_c(t):
-    return int(t.split(':')[0])*60 + int(t.split(':')[1])
-
-def change(x):
-    exc = {'C#':'1','D#':'2', 'F#':'3', 'G#':'4', 'A#':'5', 'B#': '6'}
-    for k, v in exc.items(): x = x.replace(k, v)
-    return x
+def change(s):
+    s = s.replace('C#', 'c').replace('D#', 'd').replace('F#', 'f').replace('G#', 'g').replace('A#', 'a').replace('B#', 'b')
+    return s
 
 def solution(m, musicinfos):
-    answer = []
+    answer = [0, '(None)']
+    m = change(m)
+    
     for info in musicinfos:
-        info = info.split(',')
-        info[3] = change(info[3])
-        T = time_c(info[1]) - time_c(info[0])
+        t1, t2, title, notes = info.split(',')
+        time = (int(t2[:2])-int(t1[:2]))*60 + int(t2[-2:])-int(t1[-2:])
+        notes = change(notes)
+        full_notes = notes * (time // len(notes)) + notes[:time % len(notes)]
         
-        if T >= len(info[3]):
-            M = info[3] * (T//len(info[3])) + info[3][:T%len(info[3])]
-        else: M = info[3][:T]
-        
-        if change(m) in M: answer.append([T, info[2]])
-        
-    if len(answer) == 0: return '(None)'
-    else: return sorted(answer, key=lambda x: -x[0])[0][1]
+        if m in full_notes and time > answer[0]: answer = [time, title]
+    
+    return answer[1]
