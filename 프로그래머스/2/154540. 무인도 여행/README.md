39176675bc4d9ea3d770150626516098b203d171
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-15 22:46:22 +0900

    [level 2] Title: 무인도 여행, Time: 9.81 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/README.md
+++ 프로그래머스/2/154540. 무인도 여행/README.md
@@ -1,10 +1,10 @@
 # [level 2] 무인도 여행 - 154540 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154540) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154540?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 25.91 ms
+메모리: 10.5 MB, 시간: 9.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 15일 22:42:18
+2024년 08월 15일 22:46:21
 
 ### 문제 설명
 
--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,27 +1,28 @@
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
+from collections import deque
 def solution(maps):
-    col, row = len(maps), len(maps[0])
-    visited = [[False]*row for _ in range(col)]
+    N, M = len(maps), len(maps[0])
+    visited = [[0]*M for _ in range(N)]
+
     answer = []
-    
-    for i in range(col) :
-        for j in range(row) :
-            if maps[i][j] != "X" and not visited[i][j] :
-                period = 0
-                q = [(j, i)]
-                
-                while q:
-                    x, y = q.pop()
-                    if visited[y][x]: continue
-                    visited[y][x] = True
-                    period += int(maps[y][x])
-                    
-                    for k in range(4):
-                        nx, ny = x + dx[k], y + dy[k]
-                        if -1 < nx < row and -1 < ny < col and maps[ny][nx] != "X" and not visited[ny][nx]: q.append((nx, ny))
-                    
-                answer.append(period)
-    
-    return sorted(answer) if answer else [-1]
+    for i in range(N):
+        for j in range(M):
+            if maps[i][j]=='X' or visited[i][j]:
+                continue
+            queue = deque()
+            queue.append((i,j))
+            visited[i][j]=1
+            n_food = int(maps[i][j])
+            while queue:
+                i0, j0 = queue.popleft()
+                for di, dj in [(1,0), (0,1), (-1,0), (0,-1)]:
+                    ni, nj = i0+di, j0+dj
+                    if 0<=ni<N and 0<=nj<M and not visited[ni][nj] and maps[ni][nj]!='X':
+                        queue.append((ni,nj))
+                        visited[ni][nj] = 1
+                        n_food += int(maps[ni][nj])
+            answer.append(n_food)
+    if not answer:
+        answer.append(-1)
+    else:
+        answer.sort()
+    return answer
