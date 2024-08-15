e2c1a22c2dee49c520487ee0851ead0321a6fcf5
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-15 22:50:19 +0900

    [level 2] Title: 무인도 여행, Time: 33.84 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
@@ -1,37 +1,39 @@
 function solution(maps) {
-    const arrMap = maps.map((string) => string.split(""));
-    const rowLength = arrMap.length - 1;
-    const columnLength = arrMap[0].length - 1;
-    const dRow = [1, 0, -1, 0];
-    const dCol = [0, 1, 0, -1];
-    const result = [];
+  const answer = [];
+  let [row, col] = [maps.length, maps[0].length];
+  let [dx, dy] = [
+    [-1, 0, 1, 0],
+    [0, -1, 0, 1],
+  ];
+  const table = maps.map((v) => v.split(""));
 
-    const bfs = (row, col, count) => {
-        if (row > rowLength || col > columnLength || row < 0 || col < 0) {
-            return count;
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (table[i][j] === "X") continue;
+      let cnt = +table[i][j];
+      table[i][j] = "X";
+      let stack = [[i, j]];
+
+      while (stack.length) {
+        let [x, y] = stack.pop();
+        for (let k = 0; k < 4; k++) {
+          let [nx, ny] = [x + dx[k], y + dy[k]];
+          if (
+            nx >= 0 &&
+            nx < row &&
+            ny >= 0 &&
+            ny < col &&
+            table[nx][ny] !== "X"
+          ) {
+            cnt += +table[nx][ny];
+            table[nx][ny] = "X";
+            stack.push([nx, ny]);
+          }
         }
-        if (arrMap[row][col] === "X") {
-            return count;
-        }
-
-        count += Number(arrMap[row][col]);
-        arrMap[row][col] = "X";
-
-        for (let i = 0; i < 4; i++) {
-            count = bfs(row + dRow[i], col + dCol[i], count);
-        }
-
-        return count;
-    };
-
-    for (let i = 0; i <= rowLength; i++) {
-        for (let j = 0; j <= columnLength; j++) {
-            if (arrMap[i][j] === "X") {
-                continue;
-            }
-            result.push(bfs(i, j, 0));
-        }
+      }
+      answer.push(cnt);
     }
-
-    return result.length ? result.sort((a, b) => a - b) : [-1];
+  }
+  answer.sort((a, b) => a - b);
+  return answer.length ? answer : [-1];
 }
