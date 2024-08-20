8704c885c56037af21904f86fc6ccb68c48f3741
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-20 23:11:37 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.50 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,42 +1,12 @@
-import itertools
-import copy
-
 def solution(expression):
-    operatorAll = ['*','+','-']
-    operator = []
-    largeResult = -1
-    for i in operatorAll :
-        if i in expression :
-            operator.append(i)
-    p = list(itertools.permutations(operator, len(operator)))
-    sperator = []
-    nums = '0123456789'
-    num = ''
-    for i in range(len(expression)) :
-        if expression[i] in nums :
-            num += expression[i]
-        else :
-            sperator.append(num)
-            sperator.append(expression[i])
-            num = ''
-    if num != '' :
-        sperator.append(num)
-
-    for i in p :
-        temp_sperator = copy.deepcopy(sperator)
-        for j in i :
-            while j in temp_sperator :
-                idx = temp_sperator.index(j)
-                if j == '*' :
-                    temp_sperator[idx-1] = str(int(temp_sperator[idx-1])*int(temp_sperator[idx+1]))
-                elif j == '+' :
-                    temp_sperator[idx-1] = str(int(temp_sperator[idx-1])+int(temp_sperator[idx+1]))
-                else :
-                    temp_sperator[idx-1] = str(int(temp_sperator[idx-1])-int(temp_sperator[idx+1]))
-
-                del temp_sperator[idx]
-                del temp_sperator[idx]
-        if largeResult < abs(int(temp_sperator[0])) :
-            largeResult = abs(int(temp_sperator[0]))
-
-    return largeResult
+    operations = [('+', '-', '*'),('+', '*', '-'),('-', '+', '*'),('-', '*', '+'),('*', '+', '-'),('*', '-', '+')]
+    answer = []
+    for op in operations:
+        a = op[0]
+        b = op[1]
+        temp_list = []
+        for e in expression.split(a):
+            temp = [f"({i})" for i in e.split(b)]
+            temp_list.append(f'({b.join(temp)})')
+        answer.append(abs(eval(a.join(temp_list))))
+    return max(answer)
