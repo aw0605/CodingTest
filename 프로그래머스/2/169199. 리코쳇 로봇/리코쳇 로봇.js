f16e448e2972b5cc3c0aed5e07c7574e315a7607
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-21 15:53:37 +0900

    [level 2] Title: 리코쳇 로봇, Time: 33.24 ms, Memory: 40 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
@@ -1,61 +1,73 @@
-const direction = [
-  [0, 1],
-  [1, 0],
-  [0, -1],
-  [-1, 0],
-];
-
-let n, m;
 function solution(board) {
-  n = board.length;
-  m = board[0].length;
-  let sx, sy, ex, ey;
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      if (board[i][j] === "R") {
-        sx = i;
-        sy = j;
-      }
-      if (board[i][j] === "G") {
-        ex = i;
-        ey = j;
-      }
+    const [ ROWS, COLS ] = [ board.length, board[0].length ];
+
+    const directions = [ [1,0], [-1,0], [0,1], [0,-1] ];
+    const isValid = (r, c) => {
+        if (r < 0 || r >= ROWS || c < 0 || c >= COLS) return false;
+        if (board[r][c] === "D") return false;
+        return true;
     }
-  }
+    const genKey = (r,c) => `${r}:${c}`
 
-  const discovered = Array.from(Array(n), () => Array(m).fill(false));
-  discovered[sx][sy] = true;
-  let queue = [[sx, sy, 0]];
-  while (queue.length !== 0) {
-    const size = queue.length;
-    let nextQueue = [];
+    let res = -1;
 
-    for (let i = 0; i < size; i++) {
-      const [x, y, cnt] = queue.pop();
+    const seen = new Array(ROWS).fill(null)
+                .map((_) => Array(COLS).fill(null).map((_) => Array(4).fill(false)));
+    const UP = 0;
+    const DOWN = 1;
+    const LEFT = 2;
+    const RIGHT = 3;
 
-      for (const [dx, dy] of direction) {
-        let nx = x + dx;
-        let ny = y + dy;
-        while (check(nx, ny) && board[nx][ny] !== "D") {
-          nx += dx;
-          ny += dy;
-        }
-        nx -= dx;
-        ny -= dy;
-        if (board[nx][ny] === "G") {
-          return cnt + 1;
-        }
-        if (!discovered[nx][ny]) {
-          discovered[nx][ny] = true;
-          nextQueue.push([nx, ny, cnt + 1]);
-        }
-      }
+    const delta2index = (dR, dC) => {
+        if (dR === 0 && dC === 1) return UP;
+        if (dR === 0 && dC === -1) return DOWN;
+        if (dR === 1 && dC === 0) return RIGHT;
+        if (dR === -1 && dC === 0) return LEFT;
     }
-    queue = nextQueue;
-  }
-  return -1;
+
+    const bfs = ( initR, initC, seen ) => {
+        const queue = [ [ initR, initC ] ];
+        let steps = 0;
+
+        while (queue.length) {
+            const len = queue.length;
+
+            for (let i = 0; i < len; i += 1) {
+                const [ r, c ] = queue.shift();
+
+                if (board[r][c] === "G") return steps;
+
+                for (const [ dR, dC ] of directions) {
+                    const index = delta2index(dR, dC);
+                    let newR = r + dR;
+                    let newC = c + dC;
+
+                    if (!isValid(newR, newC)) continue;
+                    if (seen[newR][newC][index]) continue;
+
+                    seen[newR][newC][index] = true;
+                    while (isValid(newR, newC)) {
+                        newR += dR;
+                        newC += dC;
+                    }
+                    newR -= dR;
+                    newC -= dC;
+                    queue.push([ newR, newC ]);
+                }
+            }
+
+            steps += 1;
+        }
+        return -1;
+    }
+
+    for (let r = 0; r < ROWS; r += 1) {
+        for (let c = 0; c < COLS; c += 1) {
+            if (board[r][c] !== "R") continue;
+
+            return bfs(r, c, seen);
+        }
+    }
+
+    return res;
 }
-
-const check = (nx, ny) => {
-  return 0 <= nx && nx < n && 0 <= ny && ny < m;
-};
