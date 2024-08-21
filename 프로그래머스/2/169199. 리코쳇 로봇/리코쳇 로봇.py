daf3b63382233ec9e8b8bdd4234416fd7697fab9
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-21 15:46:09 +0900

    [level 2] Title: 리코쳇 로봇, Time: 18.41 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -1,44 +1,26 @@
-from collections import deque
-
 def solution(board):
-    answer = 0
-    R = len(board)
-    C = len(board[0])
-    rx, ry = 0, 0
-    for i in range(R):
-        for j in range(C):
-            if board[i][j]=='R':
-                rx, ry = i, j
-                
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    
-    def bfs():
-        q = deque()
-        q.append((rx, ry))
-        visited = [[0]*C for _ in range(R)]
-        visited[rx][ry] = 1
-        
-        while q:
-            px, py = q.popleft()
-            if board[px][py] == 'G': return visited[px][py]
-            for i in range(4):
-                nx, ny = px, py
-                while True:
-                    nx, ny = nx+dx[i], ny+dy[i]
-                    if 0<=nx<R and 0<=ny<C and board[nx][ny]=='D':
-                        nx -= dx[i]
-                        ny -= dy[i]
-                        break
-                    if nx<0 or nx>=R or ny<0 or ny>=C:
-                        nx -= dx[i]
-                        ny -= dy[i]
-                        break
-                if not visited[nx][ny]:
-                    visited[nx][ny] = visited[px][py] + 1
-                    q.append((nx, ny))
-        return -1
-                    
-    answer = bfs()
-        
-    return answer-1 if answer > 0 else answer
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    robot_pos = None
+    target_pos = None
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 'R': robot_pos = (i, j)
+            elif board[i][j] == 'G': target_pos = (i, j)
+
+    queue = [(robot_pos, 0)]
+    visited = set()
+    visited.add(robot_pos)
+    while queue:
+        curr_pos, move_count = queue.pop(0)
+        if curr_pos == target_pos: return move_count
+        for dx, dy in directions:
+            x, y = curr_pos
+            while 0 <= x+dx < len(board) and 0 <= y+dy < len(board[0]) and board[x+dx][y+dy] != 'D':
+                x += dx
+                y += dy
+            if (x, y) not in visited:
+                visited.add((x, y))
+                queue.append(((x, y), move_count + 1))
+
+    return -1 
