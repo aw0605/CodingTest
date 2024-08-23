ab5f5dfdd2af3e1013252dcb6204490537193ae6
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-23 13:55:21 +0900

    [level 2] Title: 하노이의 탑, Time: 30.95 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,15 +1,7 @@
+def hanoi(f, t, m, n):
+    if n == 0: return []
+    return hanoi(f, m, t, n-1) + [[f, t]] + hanoi(m, t, f, n-1)
+
+
 def solution(n):
-    answer = []
-    
-    def hanoiTop(n, s, e, by):
-        if n == 1:
-            answer.append([s,e])
-            return
-        
-        hanoiTop(n-1, s, by, e);
-        answer.append([s, e]);
-        hanoiTop(n-1, by, e, s);
-        
-    hanoiTop(n, 1, 3, 2)
-    
-    return answer
+    return hanoi(1, 3, 2, n)
