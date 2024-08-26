da911ca1e42509913de013a9438e915c5bc2a1bc
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-26 19:34:51 +0900

    [level 2] Title: 광물 캐기, Time: 326.04 ms, Memory: 40 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
@@ -1,40 +1,18 @@
+const calc = (minerals, pick) => {
+    const tired = {'diamond': [1, 5, 25], 'iron': [1, 1, 5], 'stone': [1, 1, 1]};
+    return minerals.reduce((a, c) => a + tired[c][pick], 0);
+}
+
 function solution(picks, minerals) {
-    let answer = 0;
-    const total = picks.reduce((a, b) => a + b, 0);  
-    let num = total * 5;
-    
-    if (minerals.length > num) minerals = minerals.slice(0, num);
-    
-    let newMinerals = Array(Math.floor(minerals.length / 5) + 1).fill().map(() => [0, 0, 0]);
-    
-    for (let i = 0; i < minerals.length; i++) {
-        if (minerals[i] === 'diamond') newMinerals[Math.floor(i / 5)][0] += 1;
-        else if (minerals[i] === 'iron') newMinerals[Math.floor(i / 5)][1] += 1;
-        else if (minerals[i] === 'stone') newMinerals[Math.floor(i / 5)][2] += 1;
-    }
-    
-    newMinerals.sort((a, b) => b[0] - a[0] || b[1] - a[1] || b[2] - a[2]);
-    
-    for (let i = 0; i < newMinerals.length; i++) {
-        const [dia, iron, stone] = newMinerals[i];
-        for (let j = 0; j < picks.length; j++) {
-            if (picks[j] > 0) {
-                if (j === 0) {
-                    picks[j] -= 1;
-                    answer += dia + iron + stone;
-                    break;
-                } else if (j === 1) {
-                    picks[j] -= 1;
-                    answer += (5 * dia) + iron + stone;
-                    break;
-                } else if (j === 2) {
-                    picks[j] -= 1;
-                    answer += (25 * dia) + (5 * iron) + stone;
-                    break;
-                }
+    let answer = 1 << 30;
+    if (picks.reduce((p, c) => p + c)) {
+        for(let i = 0; i < 3; i++) {
+            if (picks[i] > 0) {
+                let ps = [ ...picks ];
+                ps[i]--;
+                answer = Math.min(calc(minerals.slice(0, 5), i) + solution(ps, minerals.slice(5)), answer);
             }
         }
-    }
-    
-    return answer;
+        return answer;
+    } else return 0;
 }
