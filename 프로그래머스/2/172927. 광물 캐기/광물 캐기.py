165f15cd5d7f2e50db0421f7ae8d71b9090a0200
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-26 19:18:21 +0900

    [level 2] Title: 광물 캐기, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -0,0 +1,32 @@
+def solution(picks, minerals):
+    answer = 0
+    total = sum(picks)    
+    num = total * 5
+    
+    if len(minerals) > num: minerals = minerals[:num]
+    
+    new_minerals =[[0,0,0] for _ in range((len(minerals)//5 + 1))]
+    for i in range(len(minerals)):
+        if minerals[i]=='diamond': new_minerals[i//5][0] += 1
+        elif minerals[i]=='iron': new_minerals[i//5][1] += 1
+        elif minerals[i]=='stone': new_minerals[i//5][2] += 1
+    
+    new_minerals.sort(reverse=True)
+
+    for i in new_minerals:
+        dia,iron,stone = i
+        for j in range(len(picks)):
+            if picks[j] > 0 and j == 0:
+                picks[j] -= 1
+                answer += dia + iron + stone
+                break
+            elif picks[j] > 0 and j == 1:
+                picks[j] -= 1
+                answer += (5*dia) + iron + stone
+                break
+            elif picks[j] > 0 and j == 2:
+                picks[j] -= 1
+                answer += (25*dia) + (5*iron) + stone
+                break
+    
+    return answer
