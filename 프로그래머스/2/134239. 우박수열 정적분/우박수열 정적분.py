2ec1499a3b4ebc87a53e57101d4df5c8bc3ee45a
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-26 18:51:26 +0900

    [level 2] Title: 우박수열 정적분, Time: 2.85 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.py
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.py
@@ -1,17 +1,27 @@
-def solution(k, ranges):
-    answer = []
+from itertools import accumulate
+
+def collatz(k):
     arr = [k]
-    
-    while k > 1:
+    while k != 1:
         k = k / 2 if k % 2 == 0 else k * 3 + 1
         arr.append(k)
         
-    area = [0]
-    for i in range(len(arr)-1): area.append(area[-1]+(arr[i]+arr[i+1])/2)
-        
-    for a, b in ranges:
-        if a >= len(area) or b-1 < -len(area) or area[b-1]-area[a] < 0: 
-            answer.append(-1)
-        else: answer.append(area[b-1]-area[a])
+    return arr
+
+def solution(k, ranges):
+    answer = []
+    ubak = collatz(k)
+    area = []
+    
+    for i in range(len(ubak) - 1): area.append((ubak[i] + ubak[i+1])/2)
+    
+    area = [0] + list(accumulate(area))
+    n = len(area) - 1
+
+    for a,b in ranges:
+        k = n + b
+        if a > k: answer.append(-1)
+        elif a == k: answer.append(0)
+        else: answer.append(area[k] - area[a])
         
     return answer
