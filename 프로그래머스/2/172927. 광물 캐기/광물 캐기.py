d5996a2699ca3f60195a1e9c3b94ad3b9e0d49ee
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-26 19:26:42 +0900

    [level 2] Title: 광물 캐기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -1,32 +1,27 @@
 def solution(picks, minerals):
     answer = 0
-    total = sum(picks)    
-    num = total * 5
+    m_score = []
+    m_groups = []
     
-    if len(minerals) > num: minerals = minerals[:num]
-    
-    new_minerals =[[0,0,0] for _ in range((len(minerals)//5 + 1))]
-    for i in range(len(minerals)):
-        if minerals[i]=='diamond': new_minerals[i//5][0] += 1
-        elif minerals[i]=='iron': new_minerals[i//5][1] += 1
-        elif minerals[i]=='stone': new_minerals[i//5][2] += 1
-    
-    new_minerals.sort(reverse=True)
+    for i in minerals[:5*sum(picks)]:
+        if i == "diamond": m_score.append(25)
+        elif i == "iron": m_score.append(5)
+        else: m_score.append(1)
 
-    for i in new_minerals:
-        dia,iron,stone = i
-        for j in range(len(picks)):
-            if picks[j] > 0 and j == 0:
-                picks[j] -= 1
-                answer += dia + iron + stone
-                break
-            elif picks[j] > 0 and j == 1:
-                picks[j] -= 1
-                answer += (5*dia) + iron + stone
-                break
-            elif picks[j] > 0 and j == 2:
-                picks[j] -= 1
-                answer += (25*dia) + (5*iron) + stone
-                break
-    
+    for i in range(len(m_score)//5+1): m_groups.append(m_score[5*i:5*(i+1)])
+
+    m_groups.sort(key= lambda x: sum(x), reverse=True)
+
+    for i in range(len(picks)) :
+        while (picks[i] != 0 and len(m_groups) != 0):
+            if i == 0: answer += len(m_groups[0])
+            elif i == 1:
+                for j in m_groups[0]:
+                    if j == 1: answer += 1
+                    else: answer += j//5
+            else: answer += sum(m_groups[0])
+            
+            picks[i] -= 1
+            del m_groups[0]
+
     return answer
