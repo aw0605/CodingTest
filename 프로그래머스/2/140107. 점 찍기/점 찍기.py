5b54c8f32cf5a5976e90f729b30b7e71009705a2
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-27 16:35:03 +0900

    [level 2] Title: 점 찍기, Time: 750.64 ms, Memory: 48.8 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.py
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.py
@@ -1,6 +1,2 @@
 def solution(k, d):
-    answer = 0
-    for y in range(0, d, k):
-        x = (d**2 - y**2)**0.5
-        answer += x//k
-    return answer + d//k + 1
+    return sum([1 + (int((d**2 - x**2)**0.5) // k) for x in range(0, d + 1, k)])
