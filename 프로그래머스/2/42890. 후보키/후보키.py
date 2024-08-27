75064e4970892894e1f42699b1f02a4c0fc8d997
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-27 16:46:22 +0900

    [level 2] Title: 후보키, Time: 13.72 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.py
+++ 프로그래머스/2/42890. 후보키/후보키.py
@@ -1,21 +1,19 @@
-from itertools import combinations 
-
 def solution(relation):
-    row = len(relation)
-    col = len(relation[0])
-    
-    arr = []
-    for i in range(1, col+1): arr.extend(combinations(range(col),i))
-        
-    unique = []
-    for i in arr:
-        tmp = [tuple(item[key] for key in i) for item in relation]
-        if len(set(tmp)) == row: unique.append(i)
+    answer_list = list()
+    for i in range(1, 1 << len(relation[0])):
+        tmp_set = set()
+        for j in range(len(relation)):
+            tmp = ''
+            for k in range(len(relation[0])):
+                if i & (1 << k): tmp += str(relation[j][k])
+            tmp_set.add(tmp)
 
-    answer = set(unique)
-    for i in range(len(unique)) :
-        for j in range(i+1, len(unique)) :
-            if len(unique[i]) == len(set(unique[i]) & set(unique[j])) :
-                answer.discard(unique[j])
-                
-    return len(answer)
+        if len(tmp_set) == len(relation):
+            not_duplicate = True
+            for num in answer_list:
+                if (num & i) == num:
+                    not_duplicate = False
+                    break
+            if not_duplicate: answer_list.append(i)
+            
+    return len(answer_list)
