a20a47c2630810af3f15b3aefc86b164c6ddf4fc
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-27 16:28:50 +0900

    [level 2] Title: 점 찍기, Time: 9.52 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.js
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.js
@@ -1,13 +1,10 @@
 function solution(k, d) {
-    let answer = 0;
+  let answer = 0;
     
-    function maxY(x){
-        return (Math.floor(Math.sqrt(d*d-x*x)));
-    }
+  for (let x = 0; x <= d; x += k) {
+    let y = Math.sqrt(d * d - x * x);
+    answer += Math.floor(y / k) + 1;
+  }
     
-    for (var x = 0; x <= d; x+=k){
-        answer += ((Math.floor(maxY(x)/k))+1);
-    }
-    
-    return answer;
+  return answer;
 }
