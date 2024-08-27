f006812ac93f63992727922c51890ee9cac33199
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-27 16:33:12 +0900

    [level 2] Title: 점 찍기, Time: 728.44 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.py
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.py
@@ -1,12 +1,6 @@
-import math
-
 def solution(k, d):
     answer = 0
-    
-    def maxY(x):
-        return math.floor(math.sqrt(d * d - x * x))
-    
-    for x in range(0, d + 1, k):
-        answer += (math.floor(maxY(x) / k) + 1)
-    
-    return answer
+    for y in range(0, d, k):
+        x = (d**2 - y**2)**0.5
+        answer += x//k
+    return answer + d//k + 1
