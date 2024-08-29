9d630c0aca06ecbafc9e6387cae079f13e2800bb
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-29 17:27:42 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 1462.44 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,36 +1,32 @@
 from itertools import product
 
-def permutation(arr, select_num):
-    if select_num == 1: return [[v] for v in arr]
-    
-    result = []
-    for v in arr:
-        fixed = v
-        rest_arr = arr
-        permutation_arr = permutation(rest_arr, select_num - 1)
-        combine_fix = [[fixed] + p for p in permutation_arr]
-        result.extend(combine_fix)
+def solution(users, emoticons):
+    sale_percent = [10, 20, 30, 40]
+    emo_len = len(emoticons)
+    result = [0, 0]
+    cases = list(product(sale_percent, repeat=emo_len))
+
+    for cur_case in cases:
+        emoticon_plus = 0
+        sum_price = 0
+
+        for buy_percent, buy_plus in users:
+            price = 0
+            et_plus_flag = False
+            
+            for et, discount in zip(emoticons, cur_case):
+                if discount >= buy_percent: price += et * (100 - discount) / 100 
+                if price >= buy_plus:
+                    et_plus_flag = True
+                    break
+            
+            if et_plus_flag: emoticon_plus += 1
+            else: sum_price += price
+
+        if emoticon_plus > result[0]:
+            result[0] = emoticon_plus
+            result[1] = sum_price
+        elif emoticon_plus == result[0] and sum_price > result[1]:
+            result[1] = sum_price
     
     return result
-
-def solution(users, emoticons):
-    answer = []
-    permutations = list(product([10, 20, 30, 40], repeat=len(emoticons)))
-
-    for combi in permutations:
-        service = 0
-        costs = [0] * len(users)
-
-        for ci, c in enumerate(combi):
-            for ui, user in enumerate(users):
-                if user[0] <= c:
-                    costs[ui] += emoticons[ci] * (100 - c) / 100
-
-        total_sum = 0
-        for i in range(len(costs)):
-            if costs[i] < users[i][1]: total_sum += costs[i]
-            else: service += 1
-        
-        answer.append([service, total_sum])
-
-    return sorted(answer, key=lambda x: (-x[0], -x[1]))[0]
