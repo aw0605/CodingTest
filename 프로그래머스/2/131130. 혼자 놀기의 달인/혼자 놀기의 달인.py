85d6a3f940ee58ceffbbf00fba3dc385cc8f2038
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-30 00:24:31 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
@@ -1,13 +1,19 @@
 def solution(cards):
     answer = []
+    n = len(cards)
+    visited = [False] * n 
+
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = True
+            cnt, t = 1, i
+            while not visited[cards[t] - 1]:
+                cnt += 1
+                t = cards[t] - 1
+                visited[t] = True
+            answer.append(cnt)
+
+    if len(answer) <= 1: return 0
+    answer.sort(reverse=True)
     
-    for i in range(len(cards)):
-        group = 0
-        while cards[i]:
-            ni = cards[i] - 1
-            cards[i], i = 0, ni
-            group += 1
-        answer.append(group)
-        
-    answer.sort()
-    return answer[-1] * answer[-2]
+    return answer[0] * answer[1]
