7bd272d9fcc17ffd82067299d9ef3f99832063dc
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-30 00:28:24 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 1.58 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.js
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.js
@@ -1,18 +1,17 @@
 function solution(cards) {
-    let answer = [];
-    
-    for (let i = 0; i < cards.length; i++) {
-        let group = 0;
-        while (cards[i] !== 0) {
-            let ni = cards[i] - 1;
-            cards[i] = 0;
-            i = ni;
-            group++;
-        }
-        answer.push(group);
-    }
-    
-    answer.sort((a, b) => b - a);
-    
-    return answer[0] * answer[1];
+    cards = cards.map(v => v - 1)
+
+    let group = cards.map((v,i) => { 
+        let save = 0
+        return Array.from(new Set(cards.map(a => {
+            save = cards[v]
+            v = save
+            return save
+        }))).length
+    })
+
+    const result = Array.from(new Set(group.sort((a,b) => b - a)))
+    if (result[0] === cards.length) return 0
+    else if (result.length === 1) return result[0] * result[0]
+    else return result[0] * result[1]
 }
