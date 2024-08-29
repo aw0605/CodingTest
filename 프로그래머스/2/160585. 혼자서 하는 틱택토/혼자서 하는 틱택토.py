66682a824e06121698184bbf4033eb31c9e79742
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-30 00:37:42 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
@@ -1,31 +1,29 @@
-def check_tic_tac_toe(board, sign):
-    lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
+def get_count(board, kind):
+    return sum(row.count(kind) for row in board)
+
+def check_win(board, kind):
+    win_lines = [
+        [board[0][0], board[0][1], board[0][2]],
+        [board[1][0], board[1][1], board[1][2]],
+        [board[2][0], board[2][1], board[2][2]],
+        [board[0][0], board[1][0], board[2][0]],
+        [board[0][1], board[1][1], board[2][1]],
+        [board[0][2], board[1][2], board[2][2]],
+        [board[0][0], board[1][1], board[2][2]],
+        [board[0][2], board[1][1], board[2][0]],
     ]
-    
-    for a, b, c in lines:
-        if board[a] == sign and board[b] == sign and board[c] == sign: return True
-    return False
+
+    return any(all(cell == kind for cell in line) for line in win_lines)
+
+def is_board_valid(board):
+    o_count = get_count(board, "O")
+    x_count = get_count(board, "X")
+
+    if x_count > o_count or o_count > x_count + 1: return False
+    if check_win(board, "O") and o_count != x_count + 1: return False
+    if check_win(board, "X") and x_count != o_count: return False
+
+    return True
 
 def solution(board):
-    board = [val for row in board for val in row]
-    O = board.count('O')
-    X = board.count('X')
-
-    if O < X or O - X > 1: return 0
-
-    o_wins = check_tic_tac_toe(board, 'O')
-    x_wins = check_tic_tac_toe(board, 'X')
-
-    if o_wins and x_wins: return 0
-    if o_wins and O - X != 1: return 0
-    if x_wins and O != X: return 0
-
-    return 1
+    return 1 if is_board_valid(board) else 0
