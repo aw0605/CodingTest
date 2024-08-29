d08191b60fa12c047390b940c672e5dd7eee0f7d
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-29 17:33:15 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 2013.56 ms, Memory: 12.7 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,32 +1,25 @@
-from itertools import product
-
 def solution(users, emoticons):
-    sale_percent = [10, 20, 30, 40]
-    emo_len = len(emoticons)
-    result = [0, 0]
-    cases = list(product(sale_percent, repeat=emo_len))
+    answer = [0,0]
+    discount = [[]]
+    for _ in emoticons:
+        next_discount = []
+        for case in discount:
+            for per in (10, 20, 30, 40):
+                next_discount.append(case+[per])
+        discount = next_discount
 
-    for cur_case in cases:
-        emoticon_plus = 0
-        sum_price = 0
+    for case in discount:
+        result = [0,0]
+        for user in users:
+            user_price = 0
+            for per, price in zip(case,emoticons):
+                if per >= user[0]:
+                    user_price += price*(100-per)/100
+            if user_price >= user[1]:
+                result[0] += 1
+            else:
+                result[1] += user_price
+        if result > answer:
+            answer = result
 
-        for buy_percent, buy_plus in users:
-            price = 0
-            et_plus_flag = False
-            
-            for et, discount in zip(emoticons, cur_case):
-                if discount >= buy_percent: price += et * (100 - discount) / 100 
-                if price >= buy_plus:
-                    et_plus_flag = True
-                    break
-            
-            if et_plus_flag: emoticon_plus += 1
-            else: sum_price += price
-
-        if emoticon_plus > result[0]:
-            result[0] = emoticon_plus
-            result[1] = sum_price
-        elif emoticon_plus == result[0] and sum_price > result[1]:
-            result[1] = sum_price
-    
-    return result
+    return answer
