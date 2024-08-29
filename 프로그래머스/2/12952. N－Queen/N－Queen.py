74ec9870cef722171e9382ef40a3d6a9531760cf
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-29 17:47:48 +0900

    [level 2] Title: N-Queen, Time: 1581.52 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -1,26 +1,18 @@
 def solution(n):
-    answer = 0
-    NOT = 100
-    status = [NOT] * n
-
-    def is_available(status, row, col):
-        if status[col] != NOT:
-            return False
-        for idx in range(len(status)):
-            if status[idx] != NOT and abs((row - status[idx]) / (col - idx)) == 1: return False
-        return True
-
-    def dfs(n, row, status):
-        nonlocal answer
-        if row == n:
-            answer += 1
-            return
-        for col in range(n):
-            if is_available(status, row, col):
-                status[col] = row
-                dfs(n, row + 1, status)
-                status[col] = NOT
-
-    dfs(n, 0, status)
-
+    check_col = [False] * 100; check_d1 = [False] * 100; check_d2 = [False] * 100
+    
+    def process(row):
+        answer = 0
+        if row == n+1: return 1
+    
+        for i in range(1,n+1):
+            d1 = row+i; d2 = n + (row - i)
+            if check_col[i] == False and check_d1[d1] == False and check_d2[d2] == False:
+                check_col[i] = True; check_d1[d1] = True; check_d2[d2] = True
+                answer += process(row+1)
+                check_col[i] = False; check_d1[d1] = False; check_d2[d2] = False
+        return answer
+    
+    answer = process(1)
+    
     return answer
