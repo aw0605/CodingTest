a716ade68491c03a6850fdf068b6409d2c86a286
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-08-31 21:48:11 +0900

    [level 2] Title: 요격 시스템, Time: 464.80 ms, Memory: 169 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -1,12 +1,13 @@
 function solution(targets) {
     let answer = 0;
     targets.sort((a,b) => a[1] - b[1])
+    let tmp = 0
     
-    let s, e = 0
     for (let target of targets) {
-        if (target[0] >= e) {
+        if (target[0] < tmp) continue
+        else {
             answer++
-            e = target[1]
+            tmp = target[1]
         }
     }
     
