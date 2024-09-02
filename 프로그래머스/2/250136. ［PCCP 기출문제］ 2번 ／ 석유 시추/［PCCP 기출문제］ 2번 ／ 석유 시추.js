caa90a715288d3645af9f7eed419e369aa76cdec
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-09-02 18:30:45 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 109.24 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
@@ -1,39 +1,34 @@
 function solution(land) {
-    const n = land.length, m = land[0].length;
-    const dx = [0, 0, 1, -1], dy = [1, -1, 0, 0];
-    const result = Array(m + 1).fill(0);
-    const visited = Array.from({ length: n }, () => Array(m).fill(0));
+  const dir = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+  const getOilInfo = (x, y) => {
+    let oilInfo = [0, y, y];
+    let queue = [[x, y]];
 
-    function bfs(a, b) {
-        let count = 0;
-        visited[a][b] = 1;
-        const q = [[a, b]];
-        let min_y = b, max_y = b;
-
-        while (q.length) {
-            const [x, y] = q.shift();
-            min_y = Math.min(min_y, y);
-            max_y = Math.max(max_y, y);
-            count++
-            for (let i = 0; i < 4; i++) {
-                const nx = x + dx[i];
-                const ny = y + dy[i];
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-                if (visited[nx][ny] === 0 && land[nx][ny] === 1) {
-                    visited[nx][ny] = 1;
-                    q.push([nx, ny]);
-                }
-            }
-        }
-
-        for (let i = min_y; i <= max_y; i++) result[i] += count;
+    while (queue.length) {
+      let [i, j] = queue.pop();
+      if (land[i][j] === 0) continue;
+      land[i][j] = 0;
+      oilInfo[0]++;
+      if (oilInfo[1] > j) oilInfo[1] = j;
+      if (oilInfo[2] < j) oilInfo[2] = j;
+      for (let [dx, dy] of dir) {
+        let newX = i + dx, newY = j + dy;
+        if (newX >= 0 && newX < land.length && newY >= 0 && newY < land[0].length 
+            && land[newX][newY] === 1)
+          queue.push([newX, newY]);
+      }
     }
+    return oilInfo;
+  };
 
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < m; j++) {
-            if (visited[i][j] === 0 && land[i][j] === 1) bfs(i, j);
-        }
-    }
+  let values = new Array(land[0].length).fill(0);
 
-    return Math.max(...result);
+  for (let i = 0; i < land.length; i++)
+    for (let j = 0; j < land[0].length; j++)
+      if (land[i][j]) {
+        let [value, start, finish] = getOilInfo(i, j);
+        while (start <= finish) values[start++] += value;
+      }
+    
+  return Math.max(...values);
 }
