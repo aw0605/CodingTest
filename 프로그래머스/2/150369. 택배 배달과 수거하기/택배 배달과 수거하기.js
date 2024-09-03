ac3c83ca42fd6204cfae466d02380db079618f8d
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-09-03 21:16:43 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 79.69 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
@@ -1,20 +1,28 @@
 function solution(cap, n, deliveries, pickups) {
-    let answer = 0, d = 0, p = 0;
-    let pos = n - 1;
+  let answer = 0;
+  let [dIdx, pIdx] = [n - 1, n - 1];
 
-    for (let i = n - 1; i >= 0; i--) {
-        d += deliveries[i];
-        p += pickups[i];
-
-        while (d > cap || p > cap) {
-            d -= cap;
-            p -= cap;
-            answer += 2 * (pos + 1);
-            pos = i;
-        }
+  while (dIdx >= 0 || pIdx >= 0) {
+    while (deliveries[dIdx] === 0 && dIdx >= 0) dIdx--
+    while (pickups[pIdx] === 0 && pIdx >= 0) pIdx--
+      
+    let [dCap, pCap] = [cap, cap];
+    answer += Math.max(dIdx + 1, pIdx + 1) * 2;
+      
+    while (dCap > 0 && dIdx >= 0) {
+      if (deliveries[dIdx] > 0) {
+        deliveries[dIdx]--
+        dCap--
+      } else dIdx--
     }
-
-    if (d > 0 || p > 0) answer += 2 * (pos + 1);
-
-    return answer;
+      
+    while (pCap > 0 && pIdx >= 0) {
+      if (pickups[pIdx] > 0) {
+        pickups[pIdx]--
+        pCap--
+      } else pIdx--
+    }
+  }
+    
+  return answer;
 }
