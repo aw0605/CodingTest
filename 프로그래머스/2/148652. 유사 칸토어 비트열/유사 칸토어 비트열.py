65a09f9d431e1d89d5f7b1405d0f9c65ce9ab1a5
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-09-04 22:48:22 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 7741.51 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
@@ -1,17 +1,12 @@
 def solution(n, l, r):
-    def count_bit_1(num):
-        if num <= 5: return '11011'[:num].count('1')
+    answer = r-l+1
+    
+    for num in range(l-1,r):
+        while num >= 1:
+            a,b = divmod(num,5)
+            if b == 2 or a == 2:
+                answer-=1
+                break
+            num = a
 
-        base = 1
-        
-        while 5 ** (base + 1) < num: base += 1
-
-        section = num // (5 ** base)
-        remainder = num % (5 ** base)
-        answer = section * (4 ** base)
-
-        if section >= 3: answer -= (4 ** base)
-        if section == 2: return answer
-        else: return answer + count_bit_1(remainder)
-
-    return count_bit_1(r) - count_bit_1(l - 1)
+    return answer
