9040745b68d13f5ddbb6d5e5201d1b46d57a847f
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-09-05 17:35:00 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 1595.10 ms, Memory: 254 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -1,38 +1,33 @@
-from collections import defaultdict
-
-def find_centroid(graph, inDegree, outDegree):
-    for node in graph.keys():
-        if inDegree[node] == 0 and outDegree[node] >= 2: return node
-
-def count_graphs(centroid, graph, inDegree, outDegree):
-    count = [0, 0, 0]
-    visited = set()
-    
-    for start in graph[centroid]:
-        visited.add(start)
-        curr = start
-        while curr:
-            if outDegree[curr] == 0:
-                count[1] += 1
-                break
-            elif outDegree[curr] == 2:
-                count[2] += 1
-                break
-            curr = graph[curr][0]
-            if curr in visited and outDegree[curr] == 1:
-                count[0] += 1
-                break
-            visited.add(curr)
-    return [centroid] + count
+from collections import defaultdict, deque
 
 def solution(edges):
+    answer = [0, 0, 0, 0]
+    start, donut, bar, eight = 0, 1, 2, 3
     graph = defaultdict(list)
-    inDegree = defaultdict(int)
-    outDegree = defaultdict(int)
-    for start, end in edges:
-        graph[start].append(end)
-        inDegree[end] += 1
-        outDegree[start] += 1
+    degrees = defaultdict(lambda: [0, 0])
 
-    centroid = find_centroid(graph, inDegree, outDegree)
-    return count_graphs(centroid, graph, inDegree, outDegree)
+    for edge in edges:
+        a, b = edge
+        graph[a].append(b)
+
+    for node, nodes in graph.items():
+        for adjacent in nodes:
+            degrees[node][0] += 1
+            degrees[adjacent][1] += 1
+
+    for node, degree in degrees.items():
+        outd, ind = degree
+
+        if outd == 0: answer[bar] += 1
+        elif outd == 2:
+            if ind > 0: answer[eight] += 1
+            else:
+                answer[start] = node
+                root = node
+        elif outd > 2:
+            answer[start] = node
+            root = node
+
+    answer[donut] = degrees[root][0] - answer[bar] - answer[eight]
+
+    return answer
