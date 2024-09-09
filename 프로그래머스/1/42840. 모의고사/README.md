c8168a36113713ba260951e4375574b7115727ee
조예린 <a50910433@gmail.com>
2024-09-10 00:31:49 +0900

    [level 1] Title: 모의고사, Time: 6.99 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 2.17 ms
+메모리: 10.4 MB, 시간: 6.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 25일 01:25:28
+2024년 09월 10일 00:31:47
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,15 +1,13 @@
 def solution(answers):
     answer = []
-    a1 = [1,2,3,4,5]
-    a2 = [2,1,2,3,2,4,2,5]
-    a3 = [3,3,1,1,2,2,4,4,5,5]
-    cA = [0,0,0]
+    scores = [0,0,0]
+    patterns = [[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]]
     
-    for i,v in enumerate(answers):
-        if v == a1[i%5]: cA[0] += 1
-        if v == a2[i%8]: cA[1] += 1
-        if v == a3[i%10]: cA[2] += 1
+    for i, a in enumerate(answers):
+        for j, pattern in enumerate(patterns):
+            if a == pattern[i % len(pattern)]: scores[j] += 1
+            
+    for i, v in enumerate(scores):
+        if v == max(scores): answer.append(i+1)
     
-    for i,v in enumerate(cA):
-        if v == max(cA): answer.append(i+1)
     return answer
