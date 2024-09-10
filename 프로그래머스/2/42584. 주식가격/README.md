1b3e0ed9912c1eaea5d2e58a5e09b7d50ec2ac91
조예린 <a50910433@gmail.com>
2024-09-10 22:25:19 +0900

    [level 2] Title: 주식가격, Time: 26.71 ms, Memory: 19.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 16.9 MB, 시간: 90.22 ms
+메모리: 19.5 MB, 시간: 26.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 11일 20:35:52
+2024년 09월 10일 22:25:17
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,10 +1,16 @@
 def solution(prices):
-    answer = [0] * len(prices)
+    n = len(prices)
+    answer = [0] * n
+    stack = [0]
     
-    for i in range(len(prices)):
-        for j in range(i+1, len(prices)):
-            if prices[i] <= prices[j]: answer[i] += 1
-            else:
-                answer[i] += 1
-                break
+    for i in range(1,n):
+        while stack and prices[i] < prices[stack[-1]]:
+            j = stack.pop()
+            answer[j] = i - j
+        stack.append(i)
+    
+    while stack:
+        j = stack.pop()
+        answer[j] = n - 1 - j
+        
     return answer
