a0ba0aa1eb06160538127bca9b23f3ced7d1f20b
조예린 <a50910433@gmail.com>
2024-09-15 16:29:35 +0900

    [level 3] Title: 베스트앨범, Time: 0.56 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,27 +1,19 @@
 function solution(genres, plays) {
-    let answer = [];
-    const g_dict = {}, p_dict = {}
-    
-    for (let i = 0; i < genres.length; i++){
-        genre = genres[i]
-        play = plays[i]
-        if (!g_dict[genre]) {
-            g_dict[genre] = []
-            p_dict[genre] = 0
-        }
-        g_dict[genre].push([i, play])
-        p_dict[genre] += play
-    }
-    
-    let sortGenres = Object.entries(p_dict).sort((a, b) => b[1] - a[1]);
+    const dic = {};
+    const dupDic = {};
+    genres.forEach((g,i)=> dic[g] = dic[g] ?  dic[g] + plays[i] : plays[i]);
 
-    for (let [genre, _] of sortGenres) {
-        let sortedSongs = g_dict[genre].sort((a, b) => {
-            if (b[1] === a[1]) return a[0] - b[0];
-            else return b[1] - a[1];
-        });
-        answer.push(...sortedSongs.slice(0, 2).map(song => song[0]));
-    }
-    
-    return answer;
+    return genres          
+          .map((g,i)=> ({genre: g, count: plays[i], index: i}))
+          .sort((a,b)=>{               
+               if (a.genre !== b.genre) return dic[b.genre] - dic[a.genre];
+               if (a.count !== b.count) return b.count - a.count;
+               return a.index - b.index;
+           })
+           .filter(v => {
+               if (dupDic[v.genre] >= 2) return false;
+               dupDic[v.genre] = dupDic[v.genre] ? dupDic[v.genre] + 1 : 1;
+               return true;
+            })
+           .map(v => v.index);    
 }
