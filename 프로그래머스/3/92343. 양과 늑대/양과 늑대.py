398c9447011073a8fb925b42f293418b88934949
조예린 <a50910433@gmail.com>
2024-09-21 19:56:06 +0900

    [level 3] Title: 양과 늑대, Time: 9.63 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -2,23 +2,19 @@ from collections import deque
 
 def solution(info, edges):
     answer = 0
-    q = deque([(0,1,0,set())])
-    
-    def build_tree(info, edges):
-        tree = [[] for _ in range(len(info))]
-        for edge in edges: tree[edge[0]].append(edge[1])
-        return tree
-    tree = build_tree(info, edges)
+    n = len(info)
+    tree = {i: [] for i in range(n)}
+    for p, s in edges: tree[p].append(s)
+    q = deque([[0, tree[0], 1, 0]])
     
     while q:
-        cur, sheep, wolf, visited = q.popleft()
-        answer = max(answer, sheep)
-        visited.update(tree[cur])
-        for nNode in visited:
-            if info[nNode]:
-                if sheep != wolf + 1:
-                    q.append((nNode,sheep,wolf+1,visited-{nNode}))
+        now, can_move, sheep, wolf = q.popleft()
+        if answer < sheep: answer = sheep
+        for i, node in enumerate(can_move):
+            if info[node] == 1:
+                if sheep > wolf + 1:
+                    q.append([node, can_move[:i] + can_move[i + 1:] + tree[node], sheep, wolf + 1])
             else:
-                q.append((nNode,sheep+1,wolf,visited-{nNode}))
-        
+                q.append([node, can_move[:i] + can_move[i + 1:] + tree[node], sheep + 1, wolf])
+
     return answer
