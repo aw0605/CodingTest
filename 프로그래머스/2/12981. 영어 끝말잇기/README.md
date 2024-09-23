57f5eb9728cb26c07ae6836897adeeb06190de7c
조예린 <a50910433@gmail.com>
2024-09-24 01:53:44 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 9.94 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=javascript#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=python3) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.19 ms
+메모리: 9.94 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 18일 23:19:51
+2024년 09월 24일 01:53:41
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,5 +1,11 @@
 def solution(n, words):
-    for i in range(1, len(words)):
-        if words[i][0] != words[i-1][-1] or words[i] in words[:i]: 
+    words_set = set()
+    prev_word = words[0][0]
+    
+    for i,w in enumerate(words):
+        if w in words_set or w[0] != prev_word:
             return [(i%n)+1, (i//n)+1]
-    else: return [0,0]
+        words_set.add(w)
+        prev_word = w[-1]
+
+    return [0,0]
