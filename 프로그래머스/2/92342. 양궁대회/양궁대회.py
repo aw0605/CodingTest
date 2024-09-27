15be0fe1b03c09e2522fcdc781c57c7cb33a12ff
조예린 <a50910433@gmail.com>
2024-09-28 00:48:45 +0900

    [level 2] Title: 양궁대회, Time: 929.61 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -1,33 +1,30 @@
+from itertools import combinations_with_replacement
+from collections import Counter
+
 def solution(n, info):
-    global answer, result
-
-    def score(ryan):
-        s = 0
-        for i in range(11):
-            if ryan[i] == info[i] == 0:
-                continue
-            if ryan[i] > info[i]:
-                s += 10 - i
-            else:
-                s -= 10 - i
-        return s
-
-    def dfs(idx, left, ryan):
-        global answer, result
-        if idx == -1 and left:
-            return
-        if left == 0:
-            s = score(ryan)
-            if result < s:
-                answer = ryan[:]
-                result = s
-            return
-        for i in range(left, -1, -1):
-            ryan[idx] = i
-            dfs(idx-1, left-i, ryan)
-            ryan[idx] = 0
-
-    answer = [0 for _ in range(11)]
-    result = 0
-    dfs(10, n, [0 for _ in range(11)])
-    return answer if result != 0 else [-1]
+    maxdiff, max_comb = 0, {}
+    
+    def calcScore(combi):
+        s1,s2 = 0,0
+        for i in range(1,11):
+            if info[10 - i] < combi.count(i): s1 += i
+            elif info[10 - i] > 0: s2 += i
+        return s1,s2
+    
+    def calcDiff(diff, cnt):
+        nonlocal maxdiff, max_comb
+        if diff > maxdiff:
+            max_comb = cnt
+            maxdiff = diff
+            
+    for combi in combinations_with_replacement(range(11), n):
+        cnt = Counter(combi)
+        s1,s2 = calcScore(combi)
+        diff = s1 - s2
+        calcDiff(diff, cnt)
+        
+    if maxdiff > 0:
+        answer = [0] * 11
+        for n in max_comb: answer[10 - n] = max_comb[n]
+        return answer
+    else: return [-1]
