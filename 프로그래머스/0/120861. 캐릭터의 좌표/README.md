10f0ceef552f5f918f29921872acc0677b9aa5bf
조예린 <a50910433@gmail.com>
2024-10-02 01:20:44 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/README.md
+++ 프로그래머스/0/120861. 캐릭터의 좌표/README.md
@@ -1,10 +1,10 @@
 # [level 0] 캐릭터의 좌표 - 120861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.00 ms
+메모리: 10.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 10일 21:23:50
+2024년 10월 02일 01:20:41
 
 ### 문제 설명
 
--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,9 +1,15 @@
 def solution(keyinput, board):
     x,y = 0,0
-    xLimit, yLimit = board[0]//2, board[1]//2
-    for v in keyinput:
-        if v == "left" and x > -xLimit: x -= 1
-        elif v == "right" and x < xLimit: x += 1
-        elif v == "up" and y < yLimit: y += 1
-        elif v == "down" and y > -yLimit: y -= 1
+    moves = {"up": [0,1], "down": [0,-1], "left": [-1,0], "right": [1,0]}
+    w,h = board[0]//2, board[1]//2
+    
+    def isValid(x,y, dx,dy):
+        return -w <= x+dx <= w and -h <= y+dy <= h
+    
+    for k in keyinput:
+        dx,dy = moves[k]
+        if isValid(x,y, dx,dy):
+            x += dx
+            y += dy
+            
     return [x,y]
