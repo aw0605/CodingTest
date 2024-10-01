822a81e9d18aa1a754075ae9544a9e503b0edf37
조예린 <a50910433@gmail.com>
2024-10-02 00:54:13 +0900

    [level 2] Title: 카펫, Time: 0.89 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.02 ms
+메모리: 10.2 MB, 시간: 0.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 15일 15:33:44
+2024년 10월 02일 00:54:09
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,4 +1,9 @@
 def solution(brown, yellow):
-    for i in range(1, int(yellow**0.5)+1):
-        if yellow % i == 0 and yellow/i*2 + i*2 + 4 == brown:
-            return [yellow//i+2, i+2]
+    total = brown + yellow
+    
+    for v in range(3, int(total**0.5)+1):
+        if total % v == 0:
+            h = int(total / v)
+            if brown == (h + v - 2) * 2: return [h,v]
+        
+    return []
