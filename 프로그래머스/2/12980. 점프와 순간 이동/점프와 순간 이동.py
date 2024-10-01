02311d525681d62a8211abdade1304de1647dd50
조예린 <a50910433@gmail.com>
2024-10-02 00:58:56 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,8 +1,2 @@
 def solution(n):
-    answer = 0;
-    while n > 0:
-        if not n % 2: n //= 2
-        else:
-            n -= 1
-            answer += 1
-    return answer
+    return bin(n).count("1")
