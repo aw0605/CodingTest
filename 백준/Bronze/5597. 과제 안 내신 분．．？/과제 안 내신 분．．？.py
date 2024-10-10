a7b5a071f57d4a3a6169667111f957b1b9e66075
조예린 <a50910433@gmail.com>
2024-10-11 04:54:26 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,10 +1,10 @@
 import sys
 
-arr = [0] * 31
+arr = [i+1 for i in range(30)]
 
 for _ in range(28):
-    n = int(sys.stdin.readline())
-    arr[n] = 1
-    
-for i in range(1, 31):
-    if arr[i] != 1: print(i)
+    submit = int(sys.stdin.readline())
+    arr.remove(submit)
+
+print(min(arr))
+print(max(arr))
