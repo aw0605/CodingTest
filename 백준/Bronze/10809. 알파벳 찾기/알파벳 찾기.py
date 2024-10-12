20ce437266f191ceca65bd05b51c25aa32ec9911
조예린 <a50910433@gmail.com>
2024-10-12 16:14:44 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,10 +1,7 @@
 import sys
 
 s = sys.stdin.readline().strip()
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
-            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 
-            'v', 'w', 'x', 'y', 'z']
+alphabet ='abcdefghijklmnopqrstuvwxyz'
 
 for v in alphabet:
-    i = s.find(v)
-    print(i, end=" ")
+    print(s.index(v) if v in s else -1, end=" ")
