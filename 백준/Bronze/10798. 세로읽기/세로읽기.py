613ac0bb7f46ab188915bbeacc7c99a1fb0f180b
조예린 <a50910433@gmail.com>
2024-10-16 01:08:18 +0900

    [Bronze I] Title: 세로읽기, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,13 +1,7 @@
 arr = [input() for _ in range(5)]
-ans = ""
-
 max_len = max(len(row) for row in arr)
 
-i = 0
-while i < max_len:
+for i in range(max_len):
     for j in range(5):
         if i < len(arr[j]):
-            ans += arr[j][i]
-    i += 1
-
-print(ans)
+            print(arr[j][i], end="")
