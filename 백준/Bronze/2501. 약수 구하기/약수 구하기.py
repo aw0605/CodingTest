e26d758b8a790bac19dd0b8acf5c46c64d879103
조예린 <a50910433@gmail.com>
2024-10-19 01:06:55 +0900

    [Bronze III] Title: 약수 구하기, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.py
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -1,11 +1,11 @@
-n,k = map(int, input().split())
-arr = set()
+n, k = map(int, input().split())
+arr = []
 
-for i in range(1, int(n**0.5)+1):
-    if n % i == 0: 
-        arr.add(i)
-        arr.add(n//i)
-        
-arr = sorted(arr)
-        
+for i in range(1, int(n**0.5) + 1):
+    if n % i == 0:
+        arr.append(i)
+        if i != n // i: arr.append(n // i)
+
+arr.sort()
+
 print(0 if k > len(arr) else arr[k-1])
