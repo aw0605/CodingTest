a072bfbda546d8f0bf1232eb491e1794ee04d0f7
조예린 <a50910433@gmail.com>
2024-10-25 01:28:56 +0900

    [Bronze II] Title: 블랙잭, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,20 +1,15 @@
 import sys
-from itertools import combinations
-
 input = sys.stdin.readline
 
 n,m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-comb = list(combinations(arr,3))
-
 ans = 0
 
-for v in comb:
-    cur = sum(v)
-    if cur == m:
-        ans = cur
-        break
-    elif cur > ans and cur <= m: ans = cur
+for i in range(n-2):
+    for j in range(i+1, n-1):
+        for k in range(j+1, n):
+            cur = arr[i] + arr[j] + arr[k]
+            if cur > ans and cur <= m: ans = cur
         
 print(ans)
