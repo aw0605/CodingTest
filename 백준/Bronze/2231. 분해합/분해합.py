baa316ac7e736c23af33abeebc3c0b941eab8fa9
조예린 <a50910433@gmail.com>
2024-10-25 16:57:39 +0900

    [Bronze II] Title: 분해합, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,10 +1,14 @@
 n = int(input())
+strN = str(n)
+
+i = max(1, n - 9 * len(strN))
 ans = 0
 
-for i in range(1, n):
-    num = i + sum(map(int, str(i)))
-    if num == n:
+while i < n:
+    num_sum = i + sum(map(int, str(i)))
+    if num_sum == n:
         ans = i
         break
+    i += 1
 
-print(ans if ans != 0 else 0)
+print(ans)
