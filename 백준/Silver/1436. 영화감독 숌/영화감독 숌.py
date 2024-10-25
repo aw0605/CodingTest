34bf2d07c5805f1137f01ed128a76523e10bb93c
조예린 <a50910433@gmail.com>
2024-10-26 01:33:29 +0900

    [Silver V] Title: 영화감독 숌, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,12 +1,33 @@
-n = int(input())
-cur = 666
-i = 0
+import sys
 
-while True:
-    if '666' in str(cur): i += 1
-    if i == n:
-        print(cur)
-        break
-    cur += 1
-    
-    
+input = sys.stdin.readline
+print = sys.stdout.write
+
+arr = [666]
+
+def count_six(n):
+  count = 0
+  for i in reversed(str(n)):
+    if i == '6':
+      count += 1
+    else :
+      break
+  return count
+
+n = int(input())
+count = 1
+i = 1
+
+while(count != n):
+  six_count = count_six(i)
+  if six_count:
+    for j in range(10**six_count):
+      arr.append(str(i) + "666"[:3-six_count] + str(j).zfill(six_count))
+      count += 1
+      if count == n: break
+  else:
+    arr.append(str(i) + "666")
+    count += 1
+  i += 1
+
+print('%s' % arr[n-1])
