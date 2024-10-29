6b3d5ea0c26172a4abef513540fd9d813591a925
조예린 <a50910433@gmail.com>
2024-10-29 17:38:00 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 172 ms, Memory: 42532 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cur = set()
+
+for _ in range(n):
+    name, status = input().split()
+    if status == "enter": cur.add(name)
+    elif status == "leave": cur.discard(name)
+        
+ans = sorted(cur, reverse=True)
+
+for v in ans:
+    print(v)
