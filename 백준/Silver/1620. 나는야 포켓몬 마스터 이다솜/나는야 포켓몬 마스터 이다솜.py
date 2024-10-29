5684511692d2e20b66073154eb789d80ddbd37ba
조예린 <a50910433@gmail.com>
2024-10-29 17:54:37 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 188 ms, Memory: 57740 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -9,8 +9,11 @@ for i in range(1, n+1):
     cur = input().strip()
     name_dic[cur] = i
     id_dic[i] = cur
-    
+
+ans = []
 for _ in range(m):
     quiz = input().strip()
-    if quiz.isdigit(): print(id_dic[int(quiz)])
-    else: print(name_dic[quiz])
+    if quiz.isdigit(): ans.append(id_dic[int(quiz)])
+    else: ans.append(str(name_dic[quiz]))
+
+print('\n'.join(ans))
