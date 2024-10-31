ec0758ef2fc5deabbae2a31eb599e25a83f55abc
조예린 <a50910433@gmail.com>
2024-11-01 01:50:49 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 688 ms, Memory: 56604 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -3,18 +3,26 @@ input = sys.stdin.readline
 
 t = int(input())
 
+def isPrime(n):
+    arr = [0,0] + [1] * (n+1)
+    for i in range(2, int(n**0.5)+1):
+        if arr[i]:
+            for j in range(i*i,n+1, i):
+                arr[j] = 0
+                
+    primes = [i for i,v in enumerate(arr) if v]
+    return arr, primes
+
 arr = [False]*2 + [True]*999999
 
-for i in range(2, int(1000000**0.5)+1):
-    if arr[i]:
-        for j in range(i*i, 1000001, i):
-            arr[j] = False
+arr, primes = isPrime(1000000)
 
 for _ in range(t):
     ans = 0
     n = int(input())
-    for i in range(2, n//2+1):
-        if arr[i] and arr[n-i]:
-            ans += 1
+    for v in primes:
+        x = n-v
+        if x < v: break
+        else:
+            if arr[x]: ans += 1
     print(ans)
-    
