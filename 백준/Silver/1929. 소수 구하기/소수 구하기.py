ce1ebed89c996d900d8dc3265c3043b6c5e84e6b
조예린 <a50910433@gmail.com>
2024-11-01 00:27:36 +0900

    [Silver III] Title: 소수 구하기, Time: 180 ms, Memory: 41332 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,10 +1,19 @@
 import sys
 input = sys.stdin.readline
 
+def eratos(n,m):
+    isPrime = [True] * (m+1)
+    isPrime[0] = isPrime[1] = False
+    for i in range(2, int(m**0.5)+1):
+        if isPrime[i]:
+            for j in range(i*i, m+1, i):
+                isPrime[j] = False
+    arr = [i for i in range(n,m+1) if isPrime[i]]
+    return arr
+
 n,m = map(int, input().split())
 
-for i in range(n,m+1):
-    if i == 1: continue
-    for j in range(2,int(i**0.5)+1):
-        if i % j == 0: break
-    else: print(i)
+arr = eratos(n,m)
+
+for num in arr:
+    print(num)
