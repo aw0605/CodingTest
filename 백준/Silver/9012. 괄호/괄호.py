324cbb129544760227e253c17c930d6c20fc90ae
조예린 <a50910433@gmail.com>
2024-11-02 17:42:48 +0900

    [Silver IV] Title: 괄호, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -3,20 +3,20 @@ input = sys.stdin.readline
 
 t = int(input())
 
-for _ in range(t):
-    ops = input().strip()
+for _ in range(t): 
     stack = 0
-    
+    ops = input().strip()
+    if ops[0] == ')' or ops[-1] == '(':
+        print('NO')
+        continue
     for v in ops:
-        if v == "(": stack += 1
-        elif v == ")":
+        if v == '(': 
+            stack += 1
+        elif v == ')':
             if not stack:
-                stack += 1
+                print('NO')
                 break
-            else:
-                stack -= 1
-    
-    if stack: print("NO")
-    else: print("YES")
-            
-    
+            else: stack -= 1
+    else:
+        if not stack: print('YES')
+        else: print('NO')
