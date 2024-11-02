551f9fc698f3c2a23944ce31858dc834a3dfab9c
조예린 <a50910433@gmail.com>
2024-11-02 19:07:06 +0900

    [Silver IV] Title: 큐 2, Time: 1008 ms, Memory: 175064 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -4,20 +4,23 @@ input = sys.stdin.readline
 
 n = int(input())
 q = deque()
+ans = []
 
 for _ in range(n):
     ops = input().split()
-    if ops[0] == "push": q.append(int(ops[1]))
+    if ops[0] == "push":
+        q.append(ops[1])
     elif ops[0] == "pop":
-        if q: print(q.popleft())
-        else: print(-1)
-    elif ops[0] == "size": print(len(q))
+        ans.append(q.popleft() if q else '-1')
+    elif ops[0] == "size":
+        ans.append(str(len(q)))
     elif ops[0] == "empty":
-        if not q: print(1)
-        else: print(0)
+        ans.append('1' if len(q) == 0 else '0')
     elif ops[0] == "front":
-        if not q: print(-1)
-        else: print(q[0])
+        ans.append(q[0] if q else '-1')
     elif ops[0] == "back":
-        if not q: print(-1)
-        else: print(q[-1])
+        ans.append(q[-1] if q else '-1')
+
+sys.stdout.write("\n".join(ans))
+            
+            
