2d6b2e86e5bfcc972a66b2fe347e7c2388ff0750
조예린 <a50910433@gmail.com>
2024-11-08 02:15:50 +0900

    [Silver III] Title: N과 M (3), Time: 1244 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+def dfs():
+    if len(ans) == m:
+        print(" ".join(map(str, ans)))
+        return
+    for i in range(1,n+1):
+        ans.append(i)
+        dfs()
+        ans.pop()
+
+n,m = map(int, input().split())
+ans = []
+
+dfs()
