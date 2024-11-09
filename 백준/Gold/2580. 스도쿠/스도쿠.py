2d8dbd884da2b6fb61732f972b93cb3d6e1edb6a
조예린 <a50910433@gmail.com>
2024-11-10 02:42:27 +0900

    [Gold IV] Title: 스도쿠, Time: 1812 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,28 +1,43 @@
 import sys
+input = sys.stdin.readline
 
-arr = [list(map(int, sys.stdin.readline().split())) for _ in range(9)]
-blank = [(i, j) for i in range(9) for j in range(9) if arr[i][j] == 0]
+board = [list(map(int, input().split())) for _ in range(9)]
+empty_pos = [(i, j) for i in range(9) for j in range(9) if board[i][j] == 0]
 
-def check(y, x, num):
-    for i in range(9): # 가로, 세로 체크
-        if arr[y][i] == num or arr[i][x] == num: return False
+rows = [[False] * 10 for _ in range(9)]
+cols = [[False] * 10 for _ in range(9)]
+boxes = [[False] * 10 for _ in range(9)]
+
+for i in range(9):
+    for j in range(9):
+        if board[i][j] != 0:
+            num = board[i][j]
+            rows[i][num] = True
+            cols[j][num] = True
+            boxes[(i // 3) * 3 + (j // 3)][num] = True
+
+def dfs(depth):
+    if depth == len(empty_pos): return True
     
-    for i in range(3): # 3x3영역 체크
-        for j in range(3):
-            if arr[y // 3 * 3 + i][x // 3 * 3 + j] == num: return False
-    return True
-
-def dfs(n):
-    if n == len(blank):
-        for row in arr: 
-            print(*row) 
-        exit() 
+    x, y = empty_pos[depth]
+    box_index = (x // 3) * 3 + (y // 3)
     
-    for num in range(1,10):
-        y, x = blank[n]
-        if check(y, x, num):
-            arr[y][x] = num
-            dfs(n+1)
-            arr[y][x] = 0
-            
+    for num in range(1, 10):
+        if not rows[x][num] and not cols[y][num] and not boxes[box_index][num]:
+            board[x][y] = num
+            rows[x][num] = True
+            cols[y][num] = True
+            boxes[box_index][num] = True
+
+            if dfs(depth + 1): return True
+
+            board[x][y] = 0
+            rows[x][num] = False
+            cols[y][num] = False
+            boxes[box_index][num] = False
+    return False
+
 dfs(0)
+
+for row in board:
+    print(*row)
