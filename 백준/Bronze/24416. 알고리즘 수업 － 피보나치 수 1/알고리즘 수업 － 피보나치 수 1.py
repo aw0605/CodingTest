548f8df399054a2d10b85e8c0e2a600a9836e7e7
조예린 <a50910433@gmail.com>
2024-11-11 00:27:04 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
@@ -3,25 +3,14 @@ input = sys.stdin.readline
 
 n = int(input())
 
-cnt1, cnt2 = 0, 0
-
 def fibo1(n):
-    global cnt1
-    if n == 1 or n == 2:
-        cnt1 += 1
-        return 1
-    else: return fibo1(n-1) + fibo1(n-2)
-
-memo = [0] * (n+1)
-def fibo2(n):
+    memo = [0] * (n+1)
     memo[1] = memo[2] = 1
-    global cnt2
     for i in range(3, n+1):
         memo[i] = memo[i-1] + memo[i-2]
-        cnt2 += 1
     return memo[n]
 
-fibo1(n)
-fibo2(n)
+def fibo2(n):
+    return n-2
 
-print(cnt1, cnt2)
+print(fibo1(n), fibo2(n))
