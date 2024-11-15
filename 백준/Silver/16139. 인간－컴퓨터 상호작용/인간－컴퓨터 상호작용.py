c5c3c660c9b2a58cfbb4921b9b036f3f525e80e1
조예린 <a50910433@gmail.com>
2024-11-15 18:58:37 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 752 ms, Memory: 93316 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -4,6 +4,13 @@ input = sys.stdin.readline
 s = input().strip()
 n = int(input())
 
-for _ in range(n):
-    alpha, l, r = input().strip().split()
-    print(s[int(l):int(r)+1].count(alpha))
+count = [[0] * 26]
+
+for i, v in enumerate(s):
+    count.append(count[len(count) - 1][:])
+    count[i + 1][ord(v) - 97] += 1
+    
+for _ in range(n):     
+    alpha, l, r = input().strip().split()     
+    ans = count[int(r) + 1][ord(alpha) - 97] - count[int(l)][ord(alpha) - 97]     
+    print(ans)
