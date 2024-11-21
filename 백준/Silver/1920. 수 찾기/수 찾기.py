5d81b6c05f0fb28e0b79bd4c5cf2fac685930955
조예린 <a50910433@gmail.com>
2024-11-22 01:05:16 +0900

    [Silver IV] Title: 수 찾기, Time: 164 ms, Memory: 51544 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -2,22 +2,9 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-a = list(map(int, input().split()))
+a = {int(num): 1 for num in input().split()} 
 m = int(input())
-arr = list(map(int, input().split()))
-a.sort()
+b = list(map(int, input().split()))
 
-for num in arr:
-    s, e = 0, n - 1
-    find = False
-
-    while s <= e:
-        mid = (s + e) // 2
-        if num == a[mid]:
-            find = True
-            print(1)
-            break
-        elif num > a[mid]: s = mid + 1
-        else: e = mid - 1
-
-    if not find: print(0)
+for num in b:
+    print(a.get(num, 0))
