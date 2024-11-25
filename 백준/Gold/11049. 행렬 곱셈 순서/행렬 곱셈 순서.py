2acf3e762313d720cb53f452170c4b039170e52a
조예린 <a50910433@gmail.com>
2024-11-25 20:02:08 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 2840 ms, Memory: 36532 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -3,14 +3,15 @@ input = sys.stdin.readline
 
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
+arr = [a for a, _ in arr] + [arr[-1][1]]
 
 dp = [[0]*n for _ in range(n)]
 
-for i in range(1, n):
-    for j in range(n):
-        if j + i == n: break
-        dp[j][j+i] = int(1e9)
-        for k in range(j, j+i):
-            dp[j][j+i] = min(dp[j][j+i], dp[j][k]+dp[k+1][j+i]+arr[j][0]*arr[k][1]*arr[j+i][1])
+for i in range(1,n):
+    for j in range(n-i):
+        e = j + i
+        mul = arr[j] * arr[e+1]
+        minimum =  min(x + y + z * mul for x, y, z in zip(dp[j][j:e], dp[e][j+1:e+1], arr[j+1:e+1]))
+        dp[j][e] = dp[e][j] = minimum
 
-print(dp[0][n-1])
+print(dp[0][-1])
