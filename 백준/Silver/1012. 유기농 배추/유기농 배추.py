29c619e9da0f52de8ee59409f22eef150a7eb8b0
조예린 <a50910433@gmail.com>
2024-12-01 01:54:58 +0900

    [Silver II] Title: 유기농 배추, Time: 60 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,35 +1,31 @@
 from collections import deque
 import sys
+sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 
-def bfs(x, y):
-    q = deque([(x, y)])
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
-                q.append((nx, ny))
-                graph[nx][ny] = 2
-    return 1
+def dfs(x, y):
+    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
+            graph[nx][ny] = 2
+            dfs(nx, ny)
 
 t = int(input())
 
 for _ in range(t):
     m, n, k = map(int, input().split())
     graph = [[0] * m for _ in range(n)]
-    for i in range(k):
+    for _ in range(k):
         a, b = map(int, input().split())
         graph[b][a] = 1
 
-    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
     cnt = 0
-
     for i in range(n):
         for j in range(m):
             if graph[i][j] == 1:
-                cnt += bfs(i, j)
-    
+                dfs(i, j)
+                cnt += 1
+
     print(cnt)
