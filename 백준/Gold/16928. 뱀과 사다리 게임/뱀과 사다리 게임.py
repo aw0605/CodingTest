9d7c97510defe378a64819e3a126a4c9919c4c7c
조예린 <a50910433@gmail.com>
2024-12-03 22:29:27 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 68 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -0,0 +1,37 @@
+from collections import deque, defaultdict
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+
+ladders = defaultdict() 
+for _ in range(n):
+    x,y = map(int, input().split())
+    ladders[x] = y
+
+snakes = defaultdict() 
+for _ in range(m):
+    x,y = map(int, input().split())
+    snakes[x] = y
+    
+board = [0] * 101
+visited = [0] * 101
+
+def bfs(s):
+    q = deque()
+    q.append(s)
+    visited[s] = 1
+    while q:
+        cur = q.popleft()
+        for i in range(1,7):
+            nextC = cur + i
+            if 0 < nextC <= 100 and not visited[nextC]:
+                if nextC in ladders: nextC = ladders[nextC]
+                if nextC in snakes: nextC = snakes[nextC]
+                if not visited[nextC]:
+                    board[nextC] = board[cur] + 1
+                    visited[nextC] = 1
+                    q.append(nextC)
+                    
+bfs(1)
+print(board[100])
