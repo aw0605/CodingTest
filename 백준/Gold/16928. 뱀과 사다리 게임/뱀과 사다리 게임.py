ca36ddbabf0669509f6a9f23268704cb390f147d
조예린 <a50910433@gmail.com>
2024-12-03 22:40:41 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 56 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -15,23 +15,21 @@ for _ in range(m):
     snakes[x] = y
     
 board = [0] * 101
-visited = [0] * 101
 
 def bfs(s):
     q = deque()
     q.append(s)
-    visited[s] = 1
+    board[s] = 1
     while q:
         cur = q.popleft()
         for i in range(1,7):
             nextC = cur + i
-            if 0 < nextC <= 100 and not visited[nextC]:
+            if 0 < nextC <= 100:
                 if nextC in ladders: nextC = ladders[nextC]
                 if nextC in snakes: nextC = snakes[nextC]
-                if not visited[nextC]:
+                if not board[nextC]:
                     board[nextC] = board[cur] + 1
-                    visited[nextC] = 1
                     q.append(nextC)
                     
 bfs(1)
-print(board[100])
+print(board[100] - 1)
