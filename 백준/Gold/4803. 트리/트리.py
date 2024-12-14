9f51b633ed4592440f2f394ead3290a22b9223e5
조예린 <a50910433@gmail.com>
2024-12-15 01:59:27 +0900

    [Gold IV] Title: 트리, Time: 180 ms, Memory: 37964 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.py
+++ 백준/Gold/4803. 트리/트리.py
@@ -1,37 +1,37 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
-def bfs(s):
-    q = deque([s])
-    isTree = True
-    while q:
-        cur = q.popleft()
-        if visited[cur]: isTree = False
-        visited[cur] = 1
-        for nn in graph[cur]:
-            if not visited[nn]: q.append(nn)
-    return isTree
+def dfs(s):
+    for nn in graph[s]:
+        if parent[s] == nn: continue
+        if visited[nn]: return True
+        parent[nn] = s
+        visited[nn] = 1
+        if dfs(nn): return True
+    return False
 
 case = 0
 while True:
     case += 1
     n, m = map(int, input().split())
     if n == 0 and m == 0: break
-    
+        
     graph = [[] for _ in range(n+1)]
     for _ in range(m):
         a, b = map(int, input().split())
         graph[a].append(b)
-        graph[b].append(a)
-
-    visited = [0] * (n + 1)
+        graph[b].append(a)      
+        
+    visited = [0] * (n+1)
+    parent = [-1] * (n+1)
     count = 0
 
     for node in range(1, n+1):
-        if visited[node]: continue
-        if bfs(node): count += 1
-
+        if not visited[node]:
+            parent[node] = node
+            visited[node] = 1
+            if not dfs(node): count += 1
+    
     if count == 0: print(f'Case {case}: No trees.')
     elif count == 1: print(f'Case {case}: There is one tree.')
     else: print(f'Case {case}: A forest of {count} trees.')
