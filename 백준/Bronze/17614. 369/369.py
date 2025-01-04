7839c7ca13aec64b428b1e191ac80f7f176c203d
조예린 <a50910433@gmail.com>
2025-01-05 00:38:36 +0900

    [Bronze III] Title: 369, Time: 632 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17614. 369/369.py
+++ 백준/Bronze/17614. 369/369.py
@@ -1,8 +1,6 @@
 num = int(input())
 
 ans = 0
-for n in range(1,num+1):
-    for v in str(n):
-        if v == "3" or v == "6" or v == "9": ans += 1
-            
+for n in range(1, num+1):
+    ans += str(n).count('3')+str(n).count('6')+str(n).count('9')
 print(ans)
