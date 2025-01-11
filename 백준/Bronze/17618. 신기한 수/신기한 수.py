4dc6d65eb216dd4f6fdd97a74c537588c5bfc130
조예린 <a50910433@gmail.com>
2025-01-12 01:16:36 +0900

    [Bronze III] Title: 신기한 수, Time: 2348 ms, Memory: 112188 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17618. 신기한 수/신기한 수.py
+++ 백준/Bronze/17618. 신기한 수/신기한 수.py
@@ -1,11 +1,6 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
 ans = 0
-for num in range(1,n+1):
-    i = [int(k) for k in str(num)]
-    j = sum(i)
-    if not num % j: ans += 1
-        
+for i in range(1, n+1):
+    j = sum([int(k) for k in str(i)])
+    if not i % j: ans += 1
 print(ans)
