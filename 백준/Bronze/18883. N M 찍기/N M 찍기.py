d3fbd5df03debb0a43ca7227ff2098dbafdae372
조예린 <a50910433@gmail.com>
2025-01-15 04:55:02 +0900

    [Bronze III] Title: N M 찍기, Time: 164 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/18883. N M 찍기/N M 찍기.py
+++ 백준/Bronze/18883. N M 찍기/N M 찍기.py
@@ -3,7 +3,5 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-for i in range(n):
-    for j in range(1, m + 1):
-        if j == m:  print(i * m + j)
-        else:  print(i * m + j, end=" ")
+for i in range(1, n+1):
+    print(" ".join(map(str, range((i - 1) * m + 1, i * m + 1))))
