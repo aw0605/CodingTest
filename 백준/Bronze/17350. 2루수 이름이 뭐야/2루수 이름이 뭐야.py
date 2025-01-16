a75b556aa94f76f2a15db2d93f94f89747ca3e9f
조예린 <a50910433@gmail.com>
2025-01-17 03:24:24 +0900

    [Bronze III] Title: 2루수 이름이 뭐야, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/17350. 2루수 이름이 뭐야/2루수 이름이 뭐야.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+for _ in range(n):
+    cur = input().strip()
+    if "anj" == cur:
+        print("뭐야;")
+        exit()
+        
+print("뭐야?")
