ab43ed4f205f066972f6135d27d80647df6f9ab1
조예린 <a50910433@gmail.com>
2025-01-19 18:33:16 +0900

    [Bronze III] Title: 친구 친구, Time: 828 ms, Memory: 34556 KB -BaekjoonHub

--- 백준/Bronze/10865. 친구 친구/친구 친구.py
+++ 백준/Bronze/10865. 친구 친구/친구 친구.py
@@ -1,13 +1,12 @@
 import sys
 input = sys.stdin.readline
 
-n,m = map(int, input().split())
+n,m = map(int,input().split())
 
-arr = [[] for _ in range(n+1)]
+arr = [0] * n
 for _ in range(m):
-    a,b = map(int, input().split())
-    arr[a].append(b)
-    arr[b].append(a)
-    
-for i in range(1,n+1):
-    print(len(arr[i]))
+    a,b = map(int,input().split())
+    arr[a-1] += 1
+    arr[b-1] += 1
+
+print(*arr)
