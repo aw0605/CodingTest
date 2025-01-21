2fda0adf1de7fe09cc73104aa973c82dc0fde227
조예린 <a50910433@gmail.com>
2025-01-21 17:44:23 +0900

    [Bronze I] Title: 유니크, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5533. 유니크/유니크.py
+++ 백준/Bronze/5533. 유니크/유니크.py
@@ -2,17 +2,18 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-
-for i in range(n):
-    score = 0
-    for j in range(3):
+arr = [[],[],[]]
+for _ in range(n):
+    a,b,c = list(map(int,input().split()))
+    arr[0].append(a)
+    arr[1].append(b)
+    arr[2].append(c)
+    
+scores = [0] * n 
+for i in range(3):
+    for j in range(n):
         cur = arr[i][j]
-        same = 0
-        for k in range(n):
-            if i == k: continue
-            if cur == arr[k][j]: 
-                same = 1
-                break
-        if not same: score += cur
-    print(score)
+        if arr[i].count(cur) > 1: continue
+        else: scores[j] += cur
+            
+for s in scores: print(s)
