faff2f96a3a2c648434283fc2c95621b001eaa97
조예린 <a50910433@gmail.com>
2025-01-25 01:15:57 +0900

    [Bronze I] Title: 민균이의 비밀번호, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9933. 민균이의 비밀번호/민균이의 비밀번호.py
+++ 백준/Bronze/9933. 민균이의 비밀번호/민균이의 비밀번호.py
@@ -5,8 +5,7 @@ n = int(input())
 arr = [input().strip() for _ in range(n)]
 
 for i,s in enumerate(arr):
-    for j in range(i,n):
-        if s[::-1] == arr[j]:
-            l = len(s)
-            print(l, s[l//2])
-            break
+    if s[::-1] in arr[i:]:
+        l = len(s)
+        print(l, s[l//2])
+        break
