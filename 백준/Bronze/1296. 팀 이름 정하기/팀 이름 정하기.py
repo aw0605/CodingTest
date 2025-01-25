ce28731df343a639237d1ff4317793980e6d4898
조예린 <a50910433@gmail.com>
2025-01-25 16:34:34 +0900

    [Bronze I] Title: 팀 이름 정하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1296. 팀 이름 정하기/팀 이름 정하기.py
+++ 백준/Bronze/1296. 팀 이름 정하기/팀 이름 정하기.py
@@ -3,20 +3,17 @@ input = sys.stdin.readline
 
 y = input().strip()
 n = int(input())
-arr = [input().strip() for _ in range(n)]
-
-def calc(y,t):
-    s = y + t
+arr = sorted([input().strip() for _ in range(n)])
+maxT = maxI = 0
+for i in range(n):
+    s = y + arr[i]
     L = s.count("L")
     O = s.count("O")
     V = s.count("V")
     E = s.count("E")
-    return ((L + O) * (L + V) * (L + E) * (O + V) * (O + E) * (V + E)) % 100
-    
-res = []
-for t in arr:
-    score = calc(y,t)
-    res.append((t,score))
-    
-res.sort(key=lambda x: (-x[1],x[0]))
-print(res[0][0])
+    score = ((L + O) * (L + V) * (L + E) * (O + V) * (O + E) * (V + E)) % 100
+    if maxT < score:
+        maxT = score
+        maxI = i
+
+print(arr[maxI])
