9b79ece2e28143740bf92df3887d0f62fbf7d4a9
조예린 <a50910433@gmail.com>
2025-01-27 04:15:58 +0900

    [Bronze I] Title: 스캐너, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3035. 스캐너/스캐너.py
+++ 백준/Bronze/3035. 스캐너/스캐너.py
@@ -2,13 +2,8 @@ import sys
 input = sys.stdin.readline
 
 r,c,zr,zc = map(int, input().split())
-
-ans = []
-for _ in range(r):
-    s = input().strip()
-    res = ""
-    for i in s: res += i * zc
-    for i in range(zr): ans.append(res)
-        
-for row in ans:
-    print(row)
+arr = [input().strip() for _ in range(r)]
+for i in range(r):
+    ans = ""
+    for j in range(c): ans += arr[i][j] * zc
+    for j in range(zr): print(ans)
