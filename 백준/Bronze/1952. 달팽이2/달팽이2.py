b99ff010a555d32c8642c06d1f3109f8a9aca7ea
조예린 <a50910433@gmail.com>
2025-01-26 16:14:23 +0900

    [Bronze I] Title: 달팽이2, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1952. 달팽이2/달팽이2.py
+++ 백준/Bronze/1952. 달팽이2/달팽이2.py
@@ -1,24 +1,4 @@
 m, n = map(int, input().split())
 
-visited = [[0] * n for _ in range(m)]
-visited[0][0] = 1
-
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-
-x, y = 0, 0
-d = 0
-total = 1
-ans = 0
-
-while total < m * n:
-    nx, ny = x + dx[d], y + dy[d]
-    if 0 <= nx < m and 0 <= ny < n and not visited[nx][ny]:
-        x, y = nx, ny
-        visited[x][y] = 1
-        total += 1
-    else:
-        d = (d + 1) % 4
-        ans += 1
-
-print(ans)
+if m > n: print(2*n-1)
+else: print(2*m-2)
