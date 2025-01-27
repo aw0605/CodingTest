9d2099887b5b794e7e4f26c2ac79c92de51efbbf
조예린 <a50910433@gmail.com>
2025-01-27 21:46:32 +0900

    [Bronze I] Title: 귀여운 수~ε٩(๑> ₃ <)۶з, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/17294. 귀여운 수～ε٩（๑＞ ₃ ＜）۶з/귀여운 수～ε٩（๑＞ ₃ ＜）۶з.py
+++ 백준/Bronze/17294. 귀여운 수～ε٩（๑＞ ₃ ＜）۶з/귀여운 수～ε٩（๑＞ ₃ ＜）۶з.py
@@ -1,13 +1,8 @@
 arr = list(map(int, input().strip()))
+cute = 1
+for i in range(len(arr)-1):
+    if arr[0] - arr[1] != arr[i] - arr[i+1]:
+        cute = 0
+        break
 
-if len(arr) <= 2: 
-    print("◝(⑅•ᴗ•⑅)◜..°♡ 뀌요미!!")
-    exit()
-else:
-    diff = arr[0] - arr[1]
-    for i in range(1,len(arr)-1):
-        cur = arr[i] - arr[i+1]
-        if cur != diff:
-            print("흥칫뿡!! <(￣ ﹌ ￣)>")
-            exit()
-    print("◝(⑅•ᴗ•⑅)◜..°♡ 뀌요미!!")
+print(['흥칫뿡!! <(￣ ﹌ ￣)>','◝(⑅•ᴗ•⑅)◜..°♡ 뀌요미!!'][cute])
