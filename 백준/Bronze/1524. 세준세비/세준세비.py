474c5616776aec2e10732d21bf881cc0084c9276
조예린 <a50910433@gmail.com>
2025-02-04 20:51:06 +0900

    [Bronze I] Title: 세준세비, Time: 260 ms, Memory: 42216 KB -BaekjoonHub

--- 백준/Bronze/1524. 세준세비/세준세비.py
+++ 백준/Bronze/1524. 세준세비/세준세비.py
@@ -1,3 +1,4 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
 
@@ -5,13 +6,11 @@ t = int(input())
 for _ in range(t):
     input()
     n,m = map(int, input().split())
-    sj = list(map(int, input().split()))
-    sb = list(map(int, input().split()))
-    sj.sort()
-    sb.sort()
+    sj = deque(sorted(map(int, input().split())))
+    sb = deque(sorted(map(int, input().split())))
     while sj and sb:
-        if sj[0] >= sb[0]: sb.pop(0)
-        else: sj.pop(0)
+        if sj[0] >= sb[0]: sb.popleft()
+        else: sj.popleft()
     if sj: print("S")
     elif sb: print("B")
     else: print("C")
