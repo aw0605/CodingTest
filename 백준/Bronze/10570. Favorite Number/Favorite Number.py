96a940f2a990fb7f423887f3b13e97377e055af0
조예린 <a50910433@gmail.com>
2025-02-06 17:00:42 +0900

    [Bronze I] Title: Favorite Number, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10570. Favorite Number/Favorite Number.py
+++ 백준/Bronze/10570. Favorite Number/Favorite Number.py
@@ -4,9 +4,8 @@ input = sys.stdin.readline
 n = int(input())
 for _ in range(n):
     v = int(input())
-    arr = [int(input()) for _ in range(v)]
-    maxN = [0,0]
-    for n in arr:
-        cnt = arr.count(n)
-        if maxN[0] < cnt or (maxN[0] == cnt and maxN[1] > n): maxN = [cnt, n]
-    print(maxN[1])
+    cnt = [0] * 1001
+    for _ in range(v):
+        cnt[int(input())] += 1
+    most = max(cnt)
+    print(cnt.index(most))
