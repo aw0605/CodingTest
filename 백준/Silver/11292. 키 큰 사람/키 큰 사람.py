d3e5509687034b568f15e890131f1ee5a208c923
조예린 <a50910433@gmail.com>
2025-03-19 05:30:19 +0900

    [Silver V] Title: 키 큰 사람, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11292. 키 큰 사람/키 큰 사람.py
+++ 백준/Silver/11292. 키 큰 사람/키 큰 사람.py
@@ -4,13 +4,11 @@ input = sys.stdin.readline
 while True:
     n = int(input())
     if n == 0: break
-    ans = []
+    students = {}
     maxH = 0
     for _ in range(n):
         a,b = input().strip().split()
-        b = float(b)
-        if b > maxH:
-            maxH = b
-            ans = [a]
-        elif b == maxH: ans.append(a)
-    print(" ".join(ans))
+        students[a] = float(b)
+    for a,b in students.items():
+        if b == max(students.values()): print(a, end=" ")
+    print(end="\n")
