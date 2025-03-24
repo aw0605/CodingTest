9ff3a4bc715640bcbc8b455067f5b84682ae67a4
조예린 <a50910433@gmail.com>
2025-03-25 04:31:14 +0900

    [Silver V] Title: 이름 궁합, Time: 900 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15312. 이름 궁합/이름 궁합.py
+++ 백준/Silver/15312. 이름 궁합/이름 궁합.py
@@ -1,15 +1,12 @@
+hint = {"A": 3, "B": 2, "C": 1, "D": 2, "E": 3, "F": 3, "G": 2, "H": 3, "I": 3, "J": 2, "K": 2, "L": 1, "M": 2, "N": 2, "O": 1, "P": 2, "Q": 2, "R": 2, "S": 1, "T": 2, "U": 1, "V": 1, "W": 1, "X": 2, "Y": 2, "Z": 1}
 A,B = input(), input()
-hint = [3, 2, 1, 2, 3, 3, 2, 3, 3, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1]
 
 arr = []
 for i in range(len(A)):
-    arr.append(hint[ord(A[i]) - 65])
-    arr.append(hint[ord(B[i]) - 65])
+    arr.append(hint[A[i]])
+    arr.append(hint[B[i]])
     
 while len(arr) > 2:
-    res = []
-    for i in range(1, len(arr)):
-        res.append((arr[i-1] + arr[i]) % 10)
-    arr = res
+    arr = [(arr[i] + arr[i+1]) % 10 for i in range(len(arr)-1)]
     
 print(str(arr[0])+str(arr[1]))
