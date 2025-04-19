23ecf8545b2211c68ad87d1856e3d73d9388ebd3
조예린 <a50910433@gmail.com>
2025-04-20 03:00:31 +0900

    [Silver V] Title: 비밀 편지, Time: 132 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/5426. 비밀 편지/비밀 편지.py
+++ 백준/Silver/5426. 비밀 편지/비밀 편지.py
@@ -5,8 +5,8 @@ t = int(input())
 for _ in range(t):
     s = input().strip()
     l = int(len(s) ** 0.5)
-    arr = [s[i:i+l] for i in range(0, len(s), l)]
-    for i in range(l-1,-1,-1):
-        for j in range(l):
-            print(arr[j][i], end="")
-    print("")
+    res = ""
+    for i in range(l, 0, -1):
+        for j in range(i, len(s)+1, l):
+            res += s[j-1]
+    print(res)
