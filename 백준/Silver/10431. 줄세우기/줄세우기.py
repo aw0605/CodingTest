12d922c2a8b819d2969740bde65a2e3196b07366
조예린 <a50910433@gmail.com>
2025-04-22 02:48:44 +0900

    [Silver V] Title: 줄세우기, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.py
+++ 백준/Silver/10431. 줄세우기/줄세우기.py
@@ -6,10 +6,8 @@ for _ in range(p):
     data = list(map(int, input().split()))
     t, arr = data[0], data[1:]
     cnt = 0
-    for i in range(0, len(arr)-1):
-        for j in range(i+1, len(arr)):
-            if arr[i] > arr[j]:
-                cnt += 1
-                arr[i],arr[j] = arr[j],arr[i]
+    for i in range(1, len(arr)):
+        for j in range(i):
+            if arr[j] > arr[i]: cnt += 1
                 
     print(t, cnt)
